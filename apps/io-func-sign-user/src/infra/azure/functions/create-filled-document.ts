5cc75d009899789dc0d049930f1f0c3caca5a51d
grausof <grausof@gmail.com>
2022-11-23 09:35:43 +0100

    Add create filled document

---
+++ apps/io-func-sign-user/src/infra/azure/functions/create-filled-document.ts
@@ -0,0 +1,26 @@
+import { createHandler, nopRequestDecoder } from "@pagopa/handler-kit";
+import * as azure from "@pagopa/handler-kit/lib/azure";
+
+import { success } from "@internal/io-sign/infra/http/response";
+
+import * as TE from "fp-ts/lib/TaskEither";
+import { pipe, identity } from "fp-ts/lib/function";
+
+import * as t from "io-ts";
+
+const makeInfoHandler = () =>
+  createHandler(
+    nopRequestDecoder,
+    () =>
+      TE.right({
+        message: "it works",
+      }),
+    identity,
+    success(
+      t.type({
+        message: t.string,
+      })
+    )
+  );
+
+export const run = pipe(makeInfoHandler(), azure.unsafeRun);
