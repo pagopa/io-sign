08f0cfe454c33dde7ce717e926f5204beb1a15f5
Filippo Tenaglia <filippo.tenaglia@pagopa.it>
2022-12-05 12:23:03 +0100

    sfeqs-1157: created the file issuer.ts on src/infra/cosmos and modified the file issuer.ts on src both on io-sign-func-issuer
    Signed-off-by: Filippo Tenaglia <filippo.tenaglia@pagopa.it>

---
+++ apps/io-func-sign-issuer/src/infra/azure/cosmos/issuer.ts
@@ -0,0 +1,40 @@
+import * as t from "io-ts";
+
+import * as cosmos from "@azure/cosmos";
+
+import {
+  CosmosdbModel,
+  BaseModel,
+  CosmosResource,
+} from "@pagopa/io-functions-commons/dist/src/utils/cosmosdb_model";
+
+import * as TE from "fp-ts/lib/TaskEither";
+import { pipe } from "fp-ts/lib/function";
+import { toCosmosDatabaseError } from "@internal/io-sign/infra/azure/cosmos/errors";
+import { Issuer, GetIssuerBySubscriptionId } from "../../../issuer";
+
+const NewIssuer = t.intersection([Issuer, BaseModel]);
+type NewIssuer = t.TypeOf<typeof NewIssuer>;
+
+const RetrievedIssuer = t.intersection([Issuer, CosmosResource]);
+type RetrievedIssuer = t.TypeOf<typeof RetrievedIssuer>;
+
+class IssuerModel extends CosmosdbModel<
+  Issuer,
+  NewIssuer,
+  RetrievedIssuer,
+  "subscriptionId"
+> {
+  constructor(db: cosmos.Database) {
+    super(db.container("issuers"), NewIssuer, RetrievedIssuer);
+  }
+}
+
+export const makeGetIssuerBySubscriptionId =
+  (db: cosmos.Database): GetIssuerBySubscriptionId =>
+  (subscriptionId) =>
+    pipe(
+      new IssuerModel(db),
+      (model) => model.find([subscriptionId, subscriptionId]),
+      TE.mapLeft(toCosmosDatabaseError)
+    );
--- apps/io-func-sign-issuer/src/issuer.ts
+++ apps/io-func-sign-issuer/src/issuer.ts
@@ -1,6 +1,6 @@
 import * as t from "io-ts";
 
-import { Id, newId } from "@io-sign/io-sign/id";
+import { Id } from "@io-sign/io-sign/id";
 
 import * as TE from "fp-ts/lib/TaskEither";
 import * as O from "fp-ts/lib/Option";
@@ -9,14 +9,16 @@ import { EntityNotFoundError } from "@io-sign/io-sign/error";
 export const Issuer = t.type({
   id: Id,
   subscriptionId: t.string,
+  externalId: t.string,
+  version: t.string,
+  email: t.string,
+  address: t.string,
+  description: t.string,
+  taxCode: t.string,
+  vatNumber: t.string,
 });
 
 export type Issuer = t.TypeOf<typeof Issuer>;
-
-export const newIssuer = (subscriptionId: string): Issuer => ({
-  id: newId(),
-  subscriptionId,
-});
 
 export type GetIssuerBySubscriptionId = (
   subscriptionId: Issuer["subscriptionId"]
