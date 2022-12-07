3dde3644652351cf84b2ec5e410e0942f3caf7b3
Luca Cavallaro <luca.cavallaro@pagopa.it>
2022-12-07 23:35:25 +0100

    move storage utility functions to io-sign

---
+++ packages/io-sign/src/infra/azure/storage/queue.ts
@@ -0,0 +1,38 @@
+import { QueueClient } from "@azure/storage-queue";
+
+import * as TE from "fp-ts/lib/TaskEither";
+import * as RTE from "fp-ts/lib/ReaderTaskEither";
+import * as E from "fp-ts/lib/Either";
+
+import { stringify } from "fp-ts/lib/Json";
+
+import { pipe, flow } from "fp-ts/lib/function";
+
+class StorageQueueError extends Error {
+  name = "StorageQueueError";
+  errorCode?: string;
+  constructor(errorCode?: string) {
+    super("Unable to enqueue the message.");
+    this.errorCode = errorCode;
+  }
+}
+
+const toBase64 = flow(Buffer.from, (b) => b.toString("base64"));
+
+const sendMessage = (message: string) => (queueClient: QueueClient) =>
+  pipe(
+    TE.tryCatch(() => queueClient.sendMessage(message), E.toError),
+    TE.filterOrElse(
+      (response) => response.errorCode === undefined,
+      (response) => new StorageQueueError(response.errorCode)
+    ),
+    TE.map(({ messageId }) => messageId)
+  );
+
+export const enqueue = flow(
+  stringify,
+  E.mapLeft(() => new Error("Unable to serialize the message.")),
+  E.map(toBase64),
+  RTE.fromEither,
+  RTE.chainW(sendMessage)
+);
