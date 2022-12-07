147eecddca66e2d5c713b842f61dc560125e2e8a
Luca Cavallaro <luca.cavallaro@pagopa.it>
2022-12-07 23:37:04 +0100

    implement signature request on user

---
+++ apps/io-func-sign-user/src/signature-field.ts
@@ -0,0 +1,47 @@
+import * as t from "io-ts";
+
+import { NonNegativeNumber } from "@pagopa/ts-commons/lib/numbers";
+import {
+  Clause,
+  SignatureFieldAttributes,
+  SignatureFieldToBeCreatedAttributes as AttributesWithCoordsAndSize,
+} from "@io-sign/io-sign/document";
+
+export const SignatureFieldToBeCreatedAttributes = t.type({
+  bottomLeft: t.type({
+    x: t.number,
+    y: t.number,
+  }),
+  topRight: t.type({
+    x: t.number,
+    y: t.number,
+  }),
+  page: NonNegativeNumber,
+});
+
+export type SignatureFieldToBeCreatedAttributes = t.TypeOf<
+  typeof SignatureFieldToBeCreatedAttributes
+>;
+
+export const toSignatureFieldToBeCreatedAttributes = ({
+  coordinates,
+  size,
+  page,
+}: AttributesWithCoordsAndSize): SignatureFieldToBeCreatedAttributes => ({
+  bottomLeft: coordinates,
+  topRight: {
+    x: coordinates.x + size.w,
+    y: coordinates.y + size.h,
+  },
+  page,
+});
+
+export const SignatureField = t.type({
+  attributes: t.union([
+    SignatureFieldAttributes,
+    SignatureFieldToBeCreatedAttributes,
+  ]),
+  clause: Clause,
+});
+
+export type SignatureField = t.TypeOf<typeof SignatureField>;
