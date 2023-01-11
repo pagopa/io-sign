306f32df6588aca02552d6d6e1e6b4b1a157ce14
grausof <grausof@gmail.com>
2023-01-11 17:42:16 +0100

    Add signatureRequest test

---
+++ apps/io-func-sign-user/src/__test__/signature-request.spec.ts
@@ -0,0 +1,78 @@
+import { describe, it, expect } from "@jest/globals";
+
+import { pipe } from "fp-ts/lib/function";
+import * as E from "fp-ts/lib/Either";
+
+import { newId } from "@io-sign/io-sign/id";
+import {
+  markAsRejected,
+  markAsSigned,
+  markAsWaitForQtsp,
+  SignatureRequest,
+  canBeWaitForQtsp,
+} from "../signature-request";
+
+const signatureRequest: SignatureRequest = {
+  id: newId(),
+  dossierId: newId(),
+  issuerId: newId(),
+  signerId: newId(),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  expiresAt: new Date(),
+  status: "WAIT_FOR_SIGNATURE",
+  documents: [],
+  qrCodeUrl: "https://mock/qrcode",
+};
+
+describe("signatureRequest status change", () => {
+  it('should mark a request with "WAIT_FOR_SIGNATURE" status to "WAIT_FOR_QTSP"', () => {
+    expect(
+      pipe(
+        markAsWaitForQtsp(signatureRequest),
+        E.map((marked) => expect(marked.status).toBe("WAIT_FOR_QTSP")),
+        E.isRight
+      )
+    ).toBe(true);
+  });
+
+  it('should mark a request with "REJECTED" status to "WAIT_FOR_QTSP"', () => {
+    expect(canBeWaitForQtsp(signatureRequest)).toBe(true);
+    expect(
+      pipe(
+        markAsRejected("Rejected reason")(signatureRequest),
+        E.chain(markAsWaitForQtsp),
+        E.map((marked) => expect(marked.status).toBe("WAIT_FOR_QTSP")),
+        E.isRight
+      )
+    ).toBe(true);
+  });
+
+  it('should mark a request with "WAIT_FOR_QTSP" status to "SIGNED"', () => {
+    const waitSignatureRequest: SignatureRequest = {
+      ...signatureRequest,
+      status: "WAIT_FOR_QTSP",
+    };
+    expect(
+      pipe(
+        markAsSigned(waitSignatureRequest),
+        E.map((marked) => expect(marked.status).toBe("SIGNED")),
+        E.isRight
+      )
+    ).toBe(true);
+  });
+
+  it('should not mark a request with "SIGNED" status to "WAIT_FOR_QTSP', () => {
+    const waitSignatureRequest: SignatureRequest = {
+      ...signatureRequest,
+      status: "WAIT_FOR_QTSP",
+    };
+    expect(
+      pipe(
+        markAsSigned(waitSignatureRequest),
+        E.chain(markAsWaitForQtsp),
+        E.isRight
+      )
+    ).toBe(false);
+  });
+});
