88249c7340d036472e358b3c5f61690c18a7773a
grausof <grausof@gmail.com>
2023-02-17 09:04:35 +0100

    Add comment to fiscal_code

--- apps/io-func-sign-user/src/infra/http/decoder/fiscal-code.ts
+++ apps/io-func-sign-user/src/infra/http/decoder/fiscal-code.ts
@@ -12,6 +12,9 @@ import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
 export const requireFiscalCode = (req: HttpRequest) =>
   pipe(
     req,
+    /* This header name (not in `kebab-case`) comes from the specification of messages with attachments that we must respect.
+     * https://github.com/pagopa/io-backend/blob/c7257c946b99830fe8c052d4cc0d4dc78b000d51/openapi/consumed/api-third-party.yaml#L94
+     */
     header("fiscal_code"),
     E.fromOption(
       () => new HttpBadRequestError("Missing fiscal_code in header")
