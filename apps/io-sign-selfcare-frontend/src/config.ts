607dbc7b564352b03610458d5dbe580d95c7c38d
Francesco Grauso <grausof@gmail.com>
2023-03-03 13:53:25 +0100

    feat: Create selfcare thanks you page
    Reviewed-by: @gunzip
    Refs: SFEQS-1345 #86

---
+++ apps/io-sign-selfcare-frontend/src/config.ts
@@ -0,0 +1,49 @@
+/* this information should be generic for each product but as of today
+ * there is no common package that can be used. There is a discussion about it on the forum.
+ * https://github.com/pagopa/engineering/discussions/14
+ */
+export const CONFIG = {
+  FOOTER: {
+    LINK: {
+      PAGOPALINK: "https://www.pagopa.it/",
+      PRIVACYPOLICY: "https://www.pagopa.it/it/privacy-policy/",
+      TERMSANDCONDITIONS:
+        "https://www.pagopa.it/it/termini-e-condizioni-di-utilizzo-del-sito/",
+      protectionOfPersonalData:
+        "https://privacyportal-de.onetrust.com/webform/77f17844-04c3-4969-a11d-462ee77acbe1/9ab6533d-be4a-482e-929a-0d8d2ab29df8",
+      ABOUTUS: "https://www.pagopa.it/it/societa/chi-siamo/",
+      MEDIA: "https://www.pagopa.it/it/",
+      WORKWITHUS: "https://www.pagopa.it/it/lavora-con-noi/",
+      CERTIFICATIONS:
+        "https://www.pagopa.it/static/10ffe3b3d90ecad83d1bbebea0512188/Certificato-SGSI-PagoPA-2020.pdf",
+      INFORMATIONSECURITY:
+        "https://www.pagopa.it/static/781646994f1f8ddad2d95af3aaedac3d/Sicurezza-delle-informazioni_PagoPA-S.p.A..pdf",
+      TRANSPARENTCOMPANY:
+        "https://pagopa.portaleamministrazionetrasparente.it/",
+      DISCLOSUREPOLICY:
+        "https://www.pagopa.it/it/responsible-disclosure-policy/",
+      MODEL321:
+        "https://pagopa.portaleamministrazionetrasparente.it/pagina746_altri-contenuti.html",
+      LINKEDIN: "https://www.linkedin.com/company/pagopa/",
+      TWITTER: "https://twitter.com/pagopa",
+      INSTAGRAM: "https://www.instagram.com/pagopaspa/",
+      MEDIUM: "https://medium.com/pagopa-spa",
+    },
+  },
+  HEADER: {
+    LABEL: "PagoPA S.p.A.",
+    LINK: {
+      ROOTLINK: "https://www.pagopa.it/",
+    },
+  },
+  SELFCARE: {
+    LOGOUTLINK: "https://selfcare.pagopa.it/auth/logout",
+    DASHBOARDLINK: "https://selfcare.pagopa.it/dashboard/",
+  },
+  ASSISTANCE: {
+    MAIL: "firmaconio-tech@pagopa.it",
+  },
+  IOSIGN: {
+    NAME: "Firma con IO",
+  },
+};
