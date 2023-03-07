4c0add13832576a7ed72f40be61dccb95423fec9
grausof <grausof@gmail.com>
2023-03-07 09:59:53 +0100

    Update encode

--- apps/io-func-sign-issuer/src/infra/self-care/encoder.ts
+++ apps/io-func-sign-issuer/src/infra/self-care/encoder.ts
@@ -9,14 +9,14 @@ import { IoSignContract } from "./contract";
 export const ioSignContractToIssuer: E.Encoder<Issuer, IoSignContract> = {
   encode: ({ internalIstitutionID, institution }) => ({
     id: newId(),
-    /* Temporarily the subscriptionId is associated with the VAT number of the issuer by prepending the string "TEST-".
+    /* Temporarily the subscriptionId is associated with the VAT number of the issuer by prepending the string "TEMP-".
      * See: [IO-SIGN-RFC-SUB-ID]
      */
     subscriptionId: pipe(
       S.Monoid.concat("TEMP-", institution.taxCode)
     ) as NonEmptyString,
     internalInstitutionId: internalIstitutionID,
-    // TODO: [SFEQS-1486] This email must be replaced with the support-mail of the issuer not yet on self-care
+    // TODO: [SFEQS-1486] This email must be replaced with the support-mail of the issuer to be retrieved via api
     email: institution.digitalAddress,
     description: institution.description,
     // Initially all newly created issuer will be in a test phase.
