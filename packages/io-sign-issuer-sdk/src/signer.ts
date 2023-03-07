66b674156218a32ffb5d21c0c7e8b5a7820d8de7
danilo spinelli <danilo.spinelli@pagopa.it>
2023-03-07 11:13:02 +0100

    refactor signer

--- packages/io-sign-issuer-sdk/src/signer.ts
+++ packages/io-sign-issuer-sdk/src/signer.ts
@@ -3,10 +3,12 @@ import inquirer from "inquirer";
 import { createConfiguration } from "@io-sign/io-sign-api-client/configuration";
 import { GetSignerByFiscalCodeBody } from "@io-sign/io-sign-api-client/models/GetSignerByFiscalCodeBody";
 import { SignerApiRequestFactory } from "@io-sign/io-sign-api-client/apis/SignerApi";
-import { createResponse, EndpointResponse } from "./utilities";
+import { createResponse } from "./utilities";
 import { fiscalCodeQuestion } from "./questions";
 
-export const callSigners = async (): Promise<EndpointResponse> => {
+export const callSigners = async (): Promise<
+  ReturnType<typeof createResponse>
+> => {
   const configuration = createConfiguration();
   const apiInstance = new SignerApiRequestFactory(configuration);
 
@@ -15,7 +17,5 @@ export const callSigners = async (): Promise<EndpointResponse> => {
     fiscalCode: answerSigners.fiscalCode,
   };
 
-  return apiInstance
-    .getSignerByFiscalCode(body)
-    .then((data) => createResponse(data));
+  return apiInstance.getSignerByFiscalCode(body).then(createResponse);
 };
