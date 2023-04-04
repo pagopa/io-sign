5fafc8e323f1d22778f975a7d0f6f5409509ad08
grausof <grausof@gmail.com>
2023-04-04 11:27:09 +0200

    Add consoleLogger

--- apps/io-func-sign-user/src/infra/azure/functions/create-signature.ts
+++ apps/io-func-sign-user/src/infra/azure/functions/create-signature.ts
@@ -2,7 +2,7 @@ import { Database as CosmosDatabase } from "@azure/cosmos";
 
 import { createHandler } from "handler-kit-legacy";
 import * as azureLegacyHandler from "handler-kit-legacy/lib/azure";
-import * as azure from "@azure/functions";
+
 import { HttpRequest } from "handler-kit-legacy/lib/http";
 
 import { success, errorWithLog } from "@io-sign/io-sign/infra/http/response";
@@ -22,7 +22,7 @@ import { ContainerClient } from "@azure/storage-blob";
 import { DocumentReady } from "@io-sign/io-sign/document";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { getDocumentUrl } from "@io-sign/io-sign/infra/azure/storage/document-url";
-import { getLogger } from "@io-sign/io-sign/infra/azure/function/logger";
+import { ConsoleLogger } from "@io-sign/io-sign/infra/console-logger";
 import { GetDocumentUrl } from "@io-sign/io-sign/document-url";
 import { requireSigner } from "../../http/decoder/signer.old";
 import { CreateSignatureBody } from "../../http/models/CreateSignatureBody";
@@ -53,170 +53,154 @@ import { LollipopApiClient } from "../../lollipop/client";
 import { makeGetBase64SamlAssertion } from "../../lollipop/assertion";
 import { getSignatureFromHeaderName } from "../../lollipop/signature";
 
-const makeCreateSignatureHandler =
-  (
-    tokenizer: PdvTokenizerClientWithApiKey,
-    lollipopApiClient: LollipopApiClient,
-    db: CosmosDatabase,
-    qtspQueue: QueueClient,
-    validatedContainerClient: ContainerClient,
-    signedContainerClient: ContainerClient,
-    qtspConfig: NamirialConfig
-  ) =>
-  (ctx: azure.Context) => {
-    const logger = getLogger(ctx);
-    const getFiscalCodeBySignerId = makeGetFiscalCodeBySignerId(tokenizer);
-    const getBase64SamlAssertion =
-      makeGetBase64SamlAssertion(lollipopApiClient);
-    const getSignatureRequest = makeGetSignatureRequest(db);
-    const creatQtspSignatureRequest = makeCreateSignatureRequestWithToken()(
-      makeGetToken()
-    )(qtspConfig);
+const makeCreateSignatureHandler = (
+  tokenizer: PdvTokenizerClientWithApiKey,
+  lollipopApiClient: LollipopApiClient,
+  db: CosmosDatabase,
+  qtspQueue: QueueClient,
+  validatedContainerClient: ContainerClient,
+  signedContainerClient: ContainerClient,
+  qtspConfig: NamirialConfig
+) => {
+  const getFiscalCodeBySignerId = makeGetFiscalCodeBySignerId(tokenizer);
+  const getBase64SamlAssertion = makeGetBase64SamlAssertion(lollipopApiClient);
+  const getSignatureRequest = makeGetSignatureRequest(db);
+  const creatQtspSignatureRequest = makeCreateSignatureRequestWithToken()(
+    makeGetToken()
+  )(qtspConfig);
 
-    const insertSignature = makeInsertSignature(db);
-    const upsertSignatureRequest = makeUpsertSignatureRequest(db);
-    const notifySignature = makeNotifySignatureReadyEvent(qtspQueue);
+  const insertSignature = makeInsertSignature(db);
+  const upsertSignatureRequest = makeUpsertSignatureRequest(db);
+  const notifySignature = makeNotifySignatureReadyEvent(qtspQueue);
 
-    const getDownloadDocumentUrl: GetDocumentUrl = (document: DocumentReady) =>
-      pipe(document, getDocumentUrl("r", 10))(validatedContainerClient);
+  const getDownloadDocumentUrl: GetDocumentUrl = (document: DocumentReady) =>
+    pipe(document, getDocumentUrl("r", 10))(validatedContainerClient);
 
-    const getUploadSignedDocumentUrl: GetDocumentUrl = (
-      document: DocumentReady
-    ) => pipe(document, getDocumentUrl("racw", 10))(signedContainerClient);
+  const getUploadSignedDocumentUrl: GetDocumentUrl = (
+    document: DocumentReady
+  ) => pipe(document, getDocumentUrl("racw", 10))(signedContainerClient);
 
-    const createSignature = makeCreateSignature(
-      getFiscalCodeBySignerId,
-      creatQtspSignatureRequest,
-      insertSignature,
-      notifySignature,
-      getSignatureRequest,
-      getDownloadDocumentUrl,
-      getUploadSignedDocumentUrl,
-      upsertSignatureRequest
-    );
+  const createSignature = makeCreateSignature(
+    getFiscalCodeBySignerId,
+    creatQtspSignatureRequest,
+    insertSignature,
+    notifySignature,
+    getSignatureRequest,
+    getDownloadDocumentUrl,
+    getUploadSignedDocumentUrl,
+    upsertSignatureRequest
+  );
 
-    const requireCreateSignatureBody = flow(
-      (req: HttpRequest) => req.body,
-      validate(CreateSignatureBody),
-      E.map((body) => ({
-        email: body.email,
-        signatureRequestId: body.signature_request_id,
-      }))
-    );
+  const requireCreateSignatureBody = flow(
+    (req: HttpRequest) => req.body,
+    validate(CreateSignatureBody),
+    E.map((body) => ({
+      email: body.email,
+      signatureRequestId: body.signature_request_id,
+    }))
+  );
 
-    const requireCreateSignaturePayload: RTE.ReaderTaskEither<
-      HttpRequest,
-      Error,
-      CreateSignaturePayload
-    > = pipe(
-      sequenceS(RTE.ApplyPar)({
-        signer: RTE.fromReaderEither(requireSigner),
-        body: RTE.fromReaderEither(requireCreateSignatureBody),
-        documentsSignature: RTE.fromReaderEither(requireDocumentsSignature),
-        qtspClauses: RTE.fromReaderEither(requireQtspClauses),
-        lollipopParams: RTE.fromReaderEither(
-          requireCreateSignatureLollipopParams
-        ),
-      }),
-      RTE.chainTaskEitherK((sequence) =>
-        pipe(
-          sequenceS(TE.ApplySeq)({
-            samlAssertionBase64: getBase64SamlAssertion(
-              sequence.lollipopParams
-            ),
-            tosSignature: pipe(
-              getSignatureFromHeaderName(
-                sequence.lollipopParams.signatureInput,
-                sequence.lollipopParams.signature,
-                "x-pagopa-lollipop-custom-tos-challenge"
-              ),
-              TE.fromEither
-            ),
-            challengeSignature: pipe(
-              getSignatureFromHeaderName(
-                sequence.lollipopParams.signatureInput,
-                sequence.lollipopParams.signature,
-                "x-pagopa-lollipop-custom-sign-challenge"
-              ),
-              TE.fromEither
-            ),
-          }),
-          TE.map(
-            ({ samlAssertionBase64, tosSignature, challengeSignature }) => ({
-              ...sequence,
-              lollipopParams: {
-                ...sequence.lollipopParams,
-                samlAssertionBase64,
-                tosSignature,
-                challengeSignature,
-              },
-            })
-          )
-        )
+  const requireCreateSignaturePayload: RTE.ReaderTaskEither<
+    HttpRequest,
+    Error,
+    CreateSignaturePayload
+  > = pipe(
+    sequenceS(RTE.ApplyPar)({
+      signer: RTE.fromReaderEither(requireSigner),
+      body: RTE.fromReaderEither(requireCreateSignatureBody),
+      documentsSignature: RTE.fromReaderEither(requireDocumentsSignature),
+      qtspClauses: RTE.fromReaderEither(requireQtspClauses),
+      lollipopParams: RTE.fromReaderEither(
+        requireCreateSignatureLollipopParams
       ),
-      RTE.map(
-        ({
-          signer,
-          documentsSignature,
-          qtspClauses,
-          body: { email, signatureRequestId },
-          lollipopParams,
-        }) => ({
-          signer,
-          qtspClauses: {
-            ...qtspClauses,
-            // TODO: [SFEQS-1237] workaround for WAF
-            filledDocumentUrl: qtspClauses.filledDocumentUrl.includes(
-              "https://"
-            )
-              ? qtspClauses.filledDocumentUrl
-              : pipe(
-                  qtspClauses.filledDocumentUrl,
-                  stringFromBase64Encode,
-                  E.chainW(
-                    validate(
-                      NonEmptyString,
-                      "Invalid encoded filledDocumentUrl"
-                    )
-                  ),
-                  E.getOrElse(() => qtspClauses.filledDocumentUrl)
+    }),
+    RTE.chainTaskEitherK((sequence) =>
+      pipe(
+        sequenceS(TE.ApplySeq)({
+          samlAssertionBase64: getBase64SamlAssertion(sequence.lollipopParams),
+          tosSignature: pipe(
+            getSignatureFromHeaderName(
+              sequence.lollipopParams.signatureInput,
+              sequence.lollipopParams.signature,
+              "x-pagopa-lollipop-custom-tos-challenge"
+            ),
+            TE.fromEither
+          ),
+          challengeSignature: pipe(
+            getSignatureFromHeaderName(
+              sequence.lollipopParams.signatureInput,
+              sequence.lollipopParams.signature,
+              "x-pagopa-lollipop-custom-sign-challenge"
+            ),
+            TE.fromEither
+          ),
+        }),
+        TE.map(({ samlAssertionBase64, tosSignature, challengeSignature }) => ({
+          ...sequence,
+          lollipopParams: {
+            ...sequence.lollipopParams,
+            samlAssertionBase64,
+            tosSignature,
+            challengeSignature,
+          },
+        }))
+      )
+    ),
+    RTE.map(
+      ({
+        signer,
+        documentsSignature,
+        qtspClauses,
+        body: { email, signatureRequestId },
+        lollipopParams,
+      }) => ({
+        signer,
+        qtspClauses: {
+          ...qtspClauses,
+          // TODO: [SFEQS-1237] workaround for WAF
+          filledDocumentUrl: qtspClauses.filledDocumentUrl.includes("https://")
+            ? qtspClauses.filledDocumentUrl
+            : pipe(
+                qtspClauses.filledDocumentUrl,
+                stringFromBase64Encode,
+                E.chainW(
+                  validate(NonEmptyString, "Invalid encoded filledDocumentUrl")
                 ),
-          },
-          documentsSignature,
-          email,
-          signatureRequestId,
-          signatureValidationParams: {
-            signatureInput: lollipopParams.signatureInput,
-            publicKey: lollipopParams.publicKey,
-            samlAssertionBase64: lollipopParams.samlAssertionBase64,
-            tosSignature: lollipopParams.tosSignature,
-            challengeSignature: lollipopParams.challengeSignature,
-          },
-        })
-      )
-    );
+                E.getOrElse(() => qtspClauses.filledDocumentUrl)
+              ),
+        },
+        documentsSignature,
+        email,
+        signatureRequestId,
+        signatureValidationParams: {
+          signatureInput: lollipopParams.signatureInput,
+          publicKey: lollipopParams.publicKey,
+          samlAssertionBase64: lollipopParams.samlAssertionBase64,
+          tosSignature: lollipopParams.tosSignature,
+          challengeSignature: lollipopParams.challengeSignature,
+        },
+      })
+    )
+  );
 
-    const decodeHttpRequest = flow(
-      azureLegacyHandler.fromHttpRequest,
-      TE.fromEither,
-      TE.chain(requireCreateSignaturePayload)
-    );
+  const decodeHttpRequest = flow(
+    azureLegacyHandler.fromHttpRequest,
+    TE.fromEither,
+    TE.chain(requireCreateSignaturePayload)
+  );
 
-    const encodeHttpSuccessResponse = flow(
-      SignatureToApiModel.encode,
-      success(SignatureDetailView)
-    );
+  const encodeHttpSuccessResponse = flow(
+    SignatureToApiModel.encode,
+    success(SignatureDetailView)
+  );
 
-    return pipe(
-      ctx,
-      createHandler(
-        decodeHttpRequest,
-        createSignature,
-        errorWithLog(logger),
-        encodeHttpSuccessResponse
-      )
-    );
-  };
+  return createHandler(
+    decodeHttpRequest,
+    createSignature,
+    errorWithLog(ConsoleLogger),
+    encodeHttpSuccessResponse
+  );
+};
 
 export const makeCreateSignatureFunction = flow(
   makeCreateSignatureHandler,
