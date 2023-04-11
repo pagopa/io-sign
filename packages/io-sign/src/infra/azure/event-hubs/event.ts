9386c7c242aa1da9803c2434372f8cbafec51ad7
grausof <grausof@gmail.com>
2023-04-11 12:23:02 +0200

    Add log to send analytics

--- packages/io-sign/src/infra/azure/event-hubs/event.ts
+++ packages/io-sign/src/infra/azure/event-hubs/event.ts
@@ -3,6 +3,9 @@ import { pipe } from "fp-ts/lib/function";
 import * as TE from "fp-ts/lib/TaskEither";
 import * as E from "fp-ts/lib/Either";
 import { EventHubProducerClient } from "@azure/event-hubs";
+import * as L from "@pagopa/logger";
+import { ConsoleLogger } from "../../console-logger";
+
 import {
   createAnalyticsEvent,
   EventName,
@@ -52,5 +55,20 @@ export const makeCreateAndSendAnalyticsEvent =
       makeSendEvent(client),
       TE.map(() => signatureRequest),
       // This is a fire and forget operation
-      TE.alt(() => TE.right(signatureRequest))
+      TE.alt(() =>
+        pipe(
+          TE.right(signatureRequest),
+          TE.chainFirstIOK(() =>
+            L.error("Unable to send analytics event", {
+              eventName,
+              signatureRequest,
+            })({
+              logger: ConsoleLogger,
+            })
+          ),
+          TE.mapLeft(
+            () => new Error("Unable to send analytics event to datalake")
+          )
+        )
+      )
     );
