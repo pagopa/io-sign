836d7138f86087bfaa10828305c15134630aa565
grausof <grausof@gmail.com>
2023-04-11 17:51:51 +0200

    Add unit-test for eventHub

---
+++ packages/io-sign/src/infra/azure/__test__/event-hubs.spec.ts
@@ -0,0 +1,58 @@
+import { describe, it, expect } from "@jest/globals";
+
+import { pipe } from "fp-ts/lib/function";
+import * as E from "fp-ts/lib/Either";
+
+import { EventHubProducerClient } from "@azure/event-hubs";
+import { EmailString, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { makeCreateAndSendAnalyticsEvent } from "../event-hubs/event";
+import { newId } from "../../../id";
+import { Issuer } from "../../../issuer";
+import { SignatureRequestSigned } from "../../../signature-request";
+import { EventName } from "../../../event";
+
+const issuer: Issuer = {
+  id: newId(),
+  subscriptionId: newId(),
+  email: "issuer@io-sign-mail.it" as EmailString,
+  description: "Mocked Issuer" as NonEmptyString,
+  internalInstitutionId: newId(),
+  environment: "TEST",
+  vatNumber: "IT01234567" as NonEmptyString,
+  department: "",
+};
+
+const signatureRequest: SignatureRequestSigned = {
+  id: newId(),
+  dossierId: newId(),
+  dossierTitle: "Rilascio CIE" as NonEmptyString,
+  issuerId: newId(),
+  issuerEmail: issuer.email,
+  issuerDescription: issuer.description,
+  issuerEnvironment: issuer.environment,
+  issuerInternalInstitutionId: newId(),
+  issuerDepartment: issuer.department,
+  signerId: newId(),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  expiresAt: new Date(),
+  status: "SIGNED",
+  signedAt: new Date(),
+  documents: [],
+};
+
+const createAndSendAnalyticsEvent = makeCreateAndSendAnalyticsEvent(
+  undefined as unknown as EventHubProducerClient
+);
+
+describe("EventHubs [infra]", () => {
+  describe("createAndSendAnalyticsEvent", () => {
+    it("should always return a right TaskEither", async () => {
+      const result = pipe(
+        signatureRequest,
+        createAndSendAnalyticsEvent(EventName.SIGNATURE_CREATED)
+      );
+      await expect(result()).resolves.toStrictEqual(E.right(signatureRequest));
+    });
+  });
+});
