d137e9c68b55b313059ad48ee737915246ce07d0
Luca Cavallaro <luca.cavallaro@pagopa.it>
2023-04-18 14:33:02 +0200

    test: move to vitest
    Reviewed-by: @gunzip @silvicir 
    Refs: SFEQS-1617 #132

--- packages/io-sign/src/__test__/event.spec.ts
+++ packages/io-sign/src/__test__/event.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect } from "vitest";
 
 import { EmailString, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 
