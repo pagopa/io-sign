a967de06c30ea8446b250c3c619bdd4849524e96
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2023-04-20 17:40:34 +0200

    build options

--- packages/web-sdk/vite.config.ts
+++ packages/web-sdk/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    lib: {
+      entry: "src/io-sign.ts",
+      formats: ["es"],
+    },
     rollupOptions: {
       external: /^lit/,
     },
