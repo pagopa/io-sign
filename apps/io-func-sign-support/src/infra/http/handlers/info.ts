3a973db7a0e40e30adb4f63c4bf83b3c0399d94f
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2023-05-18 11:12:48 +0200

    add todo

--- apps/io-func-sign-support/src/infra/http/handlers/info.ts
+++ apps/io-func-sign-support/src/infra/http/handlers/info.ts
@@ -3,6 +3,7 @@ import { pipe } from "fp-ts/function";
 
 import * as H from "@pagopa/handler-kit";
 
+// TODO(SFEQS-1676): write an RTE abstraction to check the health of backing services
 export const InfoHandler = H.of((_: H.HttpRequest) =>
   pipe(RTE.right({ message: "it works!" }), RTE.map(H.successJson))
 );
