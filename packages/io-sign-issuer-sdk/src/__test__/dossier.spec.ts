7ac8f86825ccfdfa1e3e68cea8130e9f7f8f79dc
Filippo Tenaglia <filippo80@gmail.com>
2023-05-23 16:52:43 +0200

    The tests doesn't work

--- packages/io-sign-issuer-sdk/src/__test__/dossier.spec.ts
+++ packages/io-sign-issuer-sdk/src/__test__/dossier.spec.ts
@@ -1,38 +1,56 @@
 import { Configuration, DossierApi } from "@io-sign/io-sign-api-client";
 import { describe, expect, test, vi, beforeEach } from "vitest";
 import { callDossier } from "./../dossier";
-const fetchMock = vi.fn();
+import { APIMiddleware } from "../middleware";
+vi.mock('@io-sign/io-sign-api-client/src/apis/DossierApi', async () => {
+  // can import the same module inside and does not go into an infinite loop
+const dossierApiMock = await import('@io-sign/io-sign-api-client/src/apis/DossierApi')
+return {
+  default: {
+    ...dossierApiMock.DossierApi,
+    getDossier: vi.fn(),
+      createDossier: vi.fn(),
+    },
+  }
+});
+
 const configuration = new Configuration({
   basePath: "https://example.com",
   apiKey: "aaa",
   fetchApi: fetch,
-  middleware: [],
+  middleware: [new APIMiddleware()],
 });
 const dossier = {
   id: 1,
   name: "Test Dossier",
 };
 describe('Dossier APIs', () => {
-  const api = new DossierApi(configuration);
  
-beforeEach(() => {
-  fetchMock.mockReset()
-})
-
 test('makes a GET request to fetch a single dossier and returns the result', async () => {
 
-  fetchMock.mockResolvedValue(dossier)
+  vi.fn().mockResolvedValue(dossier)
+  const request = {
+    id: "aaa",
+  };
 
-  const dossierReturned = await api.getDossier("asd");
+  const dossierReturned = await callDossier(configuration, request);
 
   expect (dossierReturned).toStrictEqual(dossier)
 })
 
 test('makes a POST request to create a dossier', async () => {
+  vi.fn().mockResolvedValue(dossier)
+  const request ={
+    title: "title",
+    documentsMetadata: [
+      {
+        title: "document title",
+        signatureFields: [],
+      },
+    ],
+  };
 
-  fetchMock.mockResolvedValue(dossier)
-
-  const dossierReturned = await api.createDossier("asd");
+  const dossierReturned = await callDossier(configuration, request);
 
   expect (dossierReturned).toStrictEqual(dossier)
 
