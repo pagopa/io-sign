cd70c609a43b87e25f716c231a4d77f32c883646
Filippo Tenaglia <filippo.tenaglia@pagopa.it>
2023-05-29 12:43:18 +0200

    Changed the .gitIgnore global file, deleted the api-client and the docs folder under io-sign-issuer-sdk and modified the signature-request.spec.ts file on the same folder

--- packages/io-sign-issuer-sdk/src/__test__/signature-request.spec.ts
+++ packages/io-sign-issuer-sdk/src/__test__/signature-request.spec.ts
@@ -1,110 +1,109 @@
 import {
   ClauseTypeEnum,
-  Configuration, Document, DocumentReady, DocumentToBeUploaded, SignatureRequestApi} from "@io-sign/io-sign-api-client";
+  Configuration,
+  Document,
+  DocumentReady,
+  DocumentToBeUploaded,
+  SignatureRequestApi,
+} from "@io-sign/io-sign-api-client";
 import { describe, expect, test, vi, beforeEach } from "vitest";
 import { callSignatureRequests } from "../signature-request";
 import { callDocumentUpload } from "../upload-file";
 
-const documentToBeUploadedMocked: DocumentToBeUploaded =  {
-  "id": "XXX",
-  "metadata": {
-    "title": "Document title",
-    "signatureFields": [
+const documentToBeUploadedMocked: DocumentToBeUploaded = {
+  id: "XXX",
+  metadata: {
+    title: "Document title",
+    signatureFields: [
       {
-        "attrs": {
-          "coordinates": {
-            "x": 10,
-            "y": 10
+        attrs: {
+          coordinates: {
+            x: 10,
+            y: 10,
           },
-          "page": 0,
-          "size": {
-            "w": 10,
-            "h": 10
-          }
+          page: 0,
+          size: {
+            w: 10,
+            h: 10,
+          },
         },
-        "clause": {
-          "title": "signature",
-          "type": ClauseTypeEnum.Required
-        }
-      }
-    ]
+        clause: {
+          title: "signature",
+          type: ClauseTypeEnum.Required,
+        },
+      },
+    ],
   },
-  "createdAt": "2023-05-26T12:53:58.559Z",
-  "updatedAt": "2023-05-26T12:54:06.884Z",
-  "status": "WAIT_FOR_UPLOAD",
+  createdAt: "2023-05-26T12:53:58.559Z",
+  updatedAt: "2023-05-26T12:54:06.884Z",
+  status: "WAIT_FOR_UPLOAD",
 };
-const documentUploadedMocked: DocumentReady =  {
-  "id": "XXX",
-  "metadata": {
-    "title": "Document title",
-    "signatureFields": [
+const documentUploadedMocked: DocumentReady = {
+  id: "XXX",
+  metadata: {
+    title: "Document title",
+    signatureFields: [
       {
-        "attrs": {
-          "coordinates": {
-            "x": 10,
-            "y": 10
+        attrs: {
+          coordinates: {
+            x: 10,
+            y: 10,
           },
-          "page": 0,
-          "size": {
-            "w": 10,
-            "h": 10
-          }
+          page: 0,
+          size: {
+            w: 10,
+            h: 10,
+          },
         },
-        "clause": {
-          "title": "signature",
-          "type": ClauseTypeEnum.Required
-        }
-      }
-    ]
+        clause: {
+          title: "signature",
+          type: ClauseTypeEnum.Required,
+        },
+      },
+    ],
   },
-  "createdAt": "2023-05-26T12:53:58.559Z",
-  "updatedAt": "2023-05-26T12:54:06.884Z",
+  createdAt: "2023-05-26T12:53:58.559Z",
+  updatedAt: "2023-05-26T12:54:06.884Z",
   uploadedAt: "2023-05-26T12:54:06.884Z",
   url: "",
-  "status": "READY",
+  status: "READY",
 };
-const getSignatureRequestWithDocumentsUploadedResponse ={
-  "id": "AAA",
-  "status": "DRAFT",
-  "dossierId": "AAA",
-  "signerId": "e648cebe-8cab-4020-882a-AAA",
-  "expiresAt": "2023-08-24T12:53:58.559Z",
-  "documents": [
-    documentUploadedMocked,
-  ],
-  "createdAt": "2023-05-26T12:53:58.559Z",
-  "updatedAt": "2023-05-26T12:54:21.635Z",
+const getSignatureRequestWithDocumentsUploadedResponse = {
+  id: "AAA",
+  status: "DRAFT",
+  dossierId: "AAA",
+  signerId: "e648cebe-8cab-4020-882a-AAA",
+  expiresAt: "2023-08-24T12:53:58.559Z",
+  documents: [documentUploadedMocked],
+  createdAt: "2023-05-26T12:53:58.559Z",
+  updatedAt: "2023-05-26T12:54:21.635Z",
 };
 
-const getSignatureRequestWithWaitForSignatureStatusResponse ={
-  "id": "AAA",
-  "status": "WAIT_FOR_SIGNATURE",
-  "dossierId": "AAA",
-  "signerId": "e648cebe-8cab-4020-882a-AAA",
-  "expiresAt": "2023-08-24T12:53:58.559Z",
-  "documents": [
-    documentUploadedMocked,
-  ],
-  "createdAt": "2023-05-26T12:53:58.559Z",
-  "updatedAt": "2023-05-26T12:54:21.635Z",
+const getSignatureRequestWithWaitForSignatureStatusResponse = {
+  id: "AAA",
+  status: "WAIT_FOR_SIGNATURE",
+  dossierId: "AAA",
+  signerId: "e648cebe-8cab-4020-882a-AAA",
+  expiresAt: "2023-08-24T12:53:58.559Z",
+  documents: [documentUploadedMocked],
+  createdAt: "2023-05-26T12:53:58.559Z",
+  updatedAt: "2023-05-26T12:54:21.635Z",
 };
 
 const getSignatureResponse = {
-  "id": "AAA",
-  "status": "DRAFT",
-  "dossierId": "AAA",
-  "signerId": "e648cebe-8cab-4020-882a-AAA",
-  "expiresAt": "2023-08-24T12:53:58.559Z",
-  "documents": [
-    documentToBeUploadedMocked,
-  ],
-  "createdAt": "2023-05-26T12:53:58.559Z",
-  "updatedAt": "2023-05-26T12:54:21.635Z",
+  id: "AAA",
+  status: "DRAFT",
+  dossierId: "AAA",
+  signerId: "e648cebe-8cab-4020-882a-AAA",
+  expiresAt: "2023-08-24T12:53:58.559Z",
+  documents: [documentToBeUploadedMocked],
+  createdAt: "2023-05-26T12:53:58.559Z",
+  updatedAt: "2023-05-26T12:54:21.635Z",
 };
 
-  const getSignatureRequest = {
+const getSignatureRequest = {
   signatureRequest: {
-      id: "AAA",
+    id: "AAA",
     dossierId: "AAA",
     signerId: "AAA",
     documents: [
@@ -113,54 +112,53 @@ const getSignatureResponse = {
         signatureFields: [
           {
             clause: {
-            title: "Signature",
-            type: "REQUIRED"
+              title: "Signature",
+              type: "REQUIRED",
+            },
+            attrs: {
+              coordinates: {
+                x: 10,
+                y: 10,
+              },
+              page: 0,
+              size: {
+                w: 10,
+                h: 10,
+              },
+            },
           },
-          attrs: {
-            coordinates: {
-              x: 10,
-              y: 10
-          },
-            page: 0,
-            size: {
-              w: 10,
-              h: 10
-          }
-          }
-        }
-        ]
-      }
-    ]
-  }
-  };
-  
+        ],
+      },
+    ],
+  },
+};
 
 vi.mock("@io-sign/io-sign-api-client");
 vi.mock("./upload-file");
-const mockGetSignatureRequest = vi.spyOn(
-  SignatureRequestApi.prototype,
-  "getSignatureRequest"
-).mockImplementation(() => { return Promise.resolve(getSignatureResponse) } );
-const mockCreateSignatureRequest = vi.spyOn(
-  SignatureRequestApi.prototype,
-  "createSignatureRequest"
-).mockImplementation(() => { return Promise.resolve(getSignatureResponse) } );
-const mockGetDocumentUploadUrl = vi.spyOn(
-  SignatureRequestApi.prototype,
-  "getDocumentUploadUrl"
-).mockImplementation(() => { return Promise.resolve("www.example.com") });
-const mockSendNotification= vi.spyOn(
+const mockGetSignatureRequest = vi
+  .spyOn(SignatureRequestApi.prototype, "getSignatureRequest")
+  .mockImplementation(() => {
+    return Promise.resolve(getSignatureResponse);
+  });
+const mockCreateSignatureRequest = vi
+  .spyOn(SignatureRequestApi.prototype, "createSignatureRequest")
+  .mockImplementation(() => {
+    return Promise.resolve(getSignatureResponse);
+  });
+const mockGetDocumentUploadUrl = vi
+  .spyOn(SignatureRequestApi.prototype, "getDocumentUploadUrl")
+  .mockImplementation(() => {
+    return Promise.resolve("www.example.com");
+  });
+const mockSendNotification = vi.spyOn(
   SignatureRequestApi.prototype,
   "sendNotification"
 );
-const mockSetSignatureRequestStatus= vi.spyOn(
+const mockSetSignatureRequestStatus = vi.spyOn(
   SignatureRequestApi.prototype,
   "setSignatureRequestStatus"
 );
 //const mockCallDocumentUpload = vi.spyOn(
-//  "./upload-file",
-//  "callDocumentUpload"
-//);
 describe("Signature Request APIs", () => {
   beforeEach(() => {
     mockGetSignatureRequest.mockClear();
@@ -168,14 +166,13 @@ describe("Signature Request APIs", () => {
     mockGetDocumentUploadUrl.mockClear();
     mockSendNotification.mockClear();
     mockSetSignatureRequestStatus.mockClear();
-//    mockCallDocumentUpload.mockClear();
   });
   test("makes a call to retrieve only the signature request detail", async () => {
-    const request = { 
-      signatureRequest : { 
-        id : "aaa",
-    }
-  };
+    const request = {
+      signatureRequest: {
+        id: "aaa",
+      },
+    };
 
     await callSignatureRequests({} as Configuration, request as any);
     expect(mockGetSignatureRequest).toBeCalledTimes(3); // it should be 8 times
@@ -187,12 +184,12 @@ describe("Signature Request APIs", () => {
   });
 
   test("makes a call to do only the creation of a new signature request", async () => {
-    const request = { 
-      signatureRequest : { 
+    const request = {
+      signatureRequest: {
         signerId: "AAA",
-        dossierId: "BBB"
-    }
-  };
+        dossierId: "BBB",
+      },
+    };
 
     await callSignatureRequests({} as Configuration, request as any);
     expect(mockGetSignatureRequest).toBeCalledTimes(3); // it should be 8 times
@@ -204,14 +201,14 @@ describe("Signature Request APIs", () => {
   });
 
   test("makes a call to set the signature request status to READY", async () => {
-    const request = { 
-      signatureRequest : { 
-        id : "aaa",
-    }
-  };
-  mockGetSignatureRequest.mockImplementation(() => {
-return Promise.resolve(getSignatureRequestWithDocumentsUploadedResponse);
-  });
+    const request = {
+      signatureRequest: {
+        id: "aaa",
+      },
+    };
+    mockGetSignatureRequest.mockImplementation(() => {
+      return Promise.resolve(getSignatureRequestWithDocumentsUploadedResponse);
+    });
     await callSignatureRequests({} as Configuration, request as any);
     expect(mockGetSignatureRequest).toBeCalledTimes(3); // it should be 8 times
     expect(mockCreateSignatureRequest).not.toBeCalled;
@@ -222,14 +219,16 @@ return Promise.resolve(getSignatureRequestWithDocumentsUploadedResponse);
   });
 
   test("makes a call to send notification", async () => {
-    const request = { 
-      signatureRequest : { 
-        id : "aaa",
-    }
-  };
-  mockGetSignatureRequest.mockImplementation(() => {
-return Promise.resolve(getSignatureRequestWithWaitForSignatureStatusResponse);
-  });
+    const request = {
+      signatureRequest: {
+        id: "aaa",
+      },
+    };
+    mockGetSignatureRequest.mockImplementation(() => {
+      return Promise.resolve(
+        getSignatureRequestWithWaitForSignatureStatusResponse
+      );
+    });
     await callSignatureRequests({} as Configuration, request as any);
     expect(mockGetSignatureRequest).toBeCalledTimes(3); // it should be 8 times
     expect(mockCreateSignatureRequest).not.toBeCalled;
@@ -238,22 +237,4 @@ return Promise.resolve(getSignatureRequestWithWaitForSignatureStatusResponse);
     expect(mockSetSignatureRequestStatus).not.toBeCalled;
     expect(callDocumentUpload).not.toBeCalled;
   });
-
-  /*
-  test("makes a call to upload the documents", async () => {
-    const request = { 
-      signatureRequest : { 
-        id: "AAA",
-    },
-    documentsPaths: ["aaa"]
-  };
-
-    await callSignatureRequests({} as Configuration, request as any);
-//    expect(mockGetSignatureRequest).toBeCalledTimes(3); // it should be 8 times
-    expect(mockCreateSignatureRequest).not.toBeCalled;
-    expect(mockGetDocumentUploadUrl).toBeCalled;
-    expect(mockSendNotification).not.toBeCalled;
-    expect(callDocumentUpload).toBeCalled;
-  });
-  */
 });
