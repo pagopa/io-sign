ffeccb816a1a9bb4e07b168b1956db02e93ce103
silvicir <127971996+silvicir@users.noreply.github.com>
2023-07-24 11:26:18 +0200

    moved from jest to vitest

--- apps/io-sign-backoffice-ca/src/endpoints/__test__/add-api-key.spec.ts
+++ apps/io-sign-backoffice-ca/src/endpoints/__test__/add-api-key.spec.ts
@@ -2,9 +2,40 @@ import { NextRequest } from "next/server";
 import { ParsingError } from "@/error";
 import { addApiKey } from "../add-api-key";
 import { ApiKeyAlreadyExistsError } from "@/api-key";
+import { vi, describe, it, expect } from "vitest";
 
-jest.mock("@/infra/azure/cosmos/config", () => ({
-  getCosmosConfigFromEnvironment: jest.fn().mockReturnValue({
+const apiKeys = [
+  {
+    id: "01H54NWNGVPYFEY03W3XPB9ZDY",
+    institutionId: "a0e07d4a-9792-4af3-8175-889aead727b8",
+    displayName: "Comune di Cori - Anagrafe - Lorem Ipsum",
+    environment: "DEFAULT",
+    resourceId: "1689092259251",
+    primaryKey: "96e1a93a156b401d953b5a1b07ec534d",
+    status: "ACTIVE",
+  },
+];
+
+const mocks = { apiKeys };
+const { getCosmosClient } = vi.hoisted(() => ({
+  getCosmosClient: vi.fn().mockReturnValue({
+    database: vi.fn().mockReturnValue({
+      container: vi.fn().mockReturnValue({
+        items: {
+          query: vi.fn(() => ({
+            fetchAll: vi.fn().mockResolvedValue({
+              resources: [],
+            }),
+          })),
+          create: vi.fn().mockResolvedValue({}),
+        },
+      }),
+    }),
+  }),
+}));
+
+vi.mock("@/infra/azure/cosmos/config", () => ({
+  getCosmosConfigFromEnvironment: vi.fn().mockReturnValue({
     accountEndpoint: "accountEndpoint",
     accountKey: "accountKey",
     dbName: "dbName",
@@ -12,43 +43,44 @@ jest.mock("@/infra/azure/cosmos/config", () => ({
   }),
 }));
 
-jest.mock("@/infra/azure/cosmos/client", () => ({
-  getCosmosClient: jest.fn().mockReturnValue({
-    database: jest.fn().mockReturnValue({
-      container: jest.fn().mockReturnValue({
-        items: {
-          query: jest.fn(() => ({
-            fetchAll: jest.fn().mockResolvedValue({
-              resources: [],
-            }),
-          })),
-          create: jest.fn().mockResolvedValue({}),
-        },
-      }),
-    }),
-  }),
-}));
+vi.mock("@/infra/azure/cosmos/client", () => {
+  return {
+    getCosmosClient,
+    // getCosmosClient: vi.fn().mockReturnValue({
+    //   database: vi.fn().mockReturnValue({
+    //     container: vi.fn().mockReturnValue({
+    //       items: {
+    //         query: vi.fn(() => ({
+    //           fetchAll: vi
+    //             .fn()
+    //             .mockResolvedValueOnce({
+    //               resources: [],
+    //             })
+    //             .mockResolvedValue({
+    //               resources: mocks.apiKeys,
+    //             }),
+    //         })),
+    //         create: vi.fn().mockResolvedValue({}),
+    //       },
+    //     }),
+    //   }),
+    // }),
+  };
+});
 
-jest.mock("@/infra/azure/api-management/config", () => ({
-  getApimConfigFromEnvironment: jest.fn().mockReturnValue({
+vi.mock("@/infra/azure/api-management/config", () => ({
+  getApimConfigFromEnvironment: vi.fn().mockReturnValue({
     subscriptionId: "subscriptionId",
     resourceGroupName: "resourceGroupName",
     serviceName: "serviceName",
     productName: "productName",
   }),
-  getApimClient: jest.fn().mockReturnValue({
-    subscription: {
-      createOrUpdate: jest.fn().mockResolvedValue({
-        primaryKey: "foo",
-      }),
-    },
-  }),
 }));
 
-jest.mock("@/infra/azure/api-management/client", () => ({
-  getApimClient: jest.fn().mockReturnValue({
+vi.mock("@/infra/azure/api-management/client", () => ({
+  getApimClient: vi.fn().mockReturnValue({
     subscription: {
-      createOrUpdate: jest.fn().mockResolvedValue({
+      createOrUpdate: vi.fn().mockResolvedValue({
         primaryKey: "foo",
       }),
     },
@@ -56,23 +88,33 @@ jest.mock("@/infra/azure/api-management/client", () => ({
 }));
 
 describe("AddApiKey endpoint", () => {
-  const apiKeys = [
-    {
-      id: "01H54NWNGVPYFEY03W3XPB9ZDY",
-      institutionId: "a0e07d4a-9792-4af3-8175-889aead727b8",
-      displayName: "Comune di Cori - Anagrafe - Lorem Ipsum",
-      environment: "DEFAULT",
-      resourceId: "1689092259251",
-      primaryKey: "96e1a93a156b401d953b5a1b07ec534d",
-      status: "ACTIVE",
-    },
-  ];
+  it("should return a 400 HTTP response on input validation", async () => {
+    const request = {
+      json: vi.fn(async () => ({ foo: "foo" })),
+    } as unknown as NextRequest;
 
-  const mocks = { apiKeys };
+    expect(addApiKey(request)).rejects.toStrictEqual(
+      new ParsingError("Failed to parse request body")
+    );
+  });
 
-  // TODO: update this test
-  it.skip("should return a 409 HTTP response on input body conflict", async () => {
+  it("should return a 409 HTTP response on input body conflict", async () => {
     // these institutionId, displayName and environment are already present in the mocked API keys
+    getCosmosClient.mockReturnValueOnce({
+      database: vi.fn().mockReturnValue({
+        container: vi.fn().mockReturnValue({
+          items: {
+            query: vi.fn(() => ({
+              fetchAll: vi.fn().mockResolvedValue({
+                resources: mocks.apiKeys,
+              }),
+            })),
+            create: vi.fn().mockResolvedValue({}),
+          },
+        }),
+      }),
+    });
+
     const bodyRequest = {
       institutionId: "a0e07d4a-9792-4af3-8175-889aead727b8",
       displayName: "Comune di Cori - Anagrafe - Lorem Ipsum",
@@ -80,7 +122,7 @@ describe("AddApiKey endpoint", () => {
       resourceId: "1689092259251",
     };
     const request = {
-      json: jest.fn(async () => bodyRequest),
+      json: vi.fn(async () => bodyRequest),
     } as unknown as NextRequest;
 
     expect(addApiKey(request)).rejects.toStrictEqual(
@@ -89,6 +131,21 @@ describe("AddApiKey endpoint", () => {
   });
 
   it("should return a 201 HTTP response on success", async () => {
+    // getCosmosClient.mockReturnValue({
+    //   database: vi.fn().mockReturnValue({
+    //     container: vi.fn().mockReturnValue({
+    //       items: {
+    //         query: vi.fn(() => ({
+    //           fetchAll: vi.fn().mockResolvedValue({
+    //             resources: [],
+    //           }),
+    //         })),
+    //         create: vi.fn().mockResolvedValue({}),
+    //       },
+    //     }),
+    //   }),
+    // });
+
     const bodyRequest = {
       institutionId: "01ARZ3NDEKTSV4RRFFQ69G5FAV",
       displayName: "POLIBA - Dipartimento di Informatica (TEST)",
@@ -96,22 +153,12 @@ describe("AddApiKey endpoint", () => {
       resourceId: "1689092259251",
     };
     const request = {
-      json: jest.fn(async () => bodyRequest),
+      json: vi.fn(async () => bodyRequest),
     } as unknown as NextRequest;
 
     expect(addApiKey(request)).resolves.toEqual({
       id: expect.any(String),
       primaryKey: expect.any(String),
     });
-  });
-
-  it("should return a 400 HTTP response on input validation", async () => {
-    const request = {
-      json: jest.fn(async () => ({ foo: "foo" })),
-    } as unknown as NextRequest;
-
-    expect(addApiKey(request)).rejects.toStrictEqual(
-      new ParsingError("Failed to parse request body")
-    );
   });
 });
