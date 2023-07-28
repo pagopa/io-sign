d05d6365a03ccd3c58a87075f33074e402ca8741
silvicir <127971996+silvicir@users.noreply.github.com>
2023-07-28 14:10:37 +0200

    from 400 to 422 http error

--- apps/io-sign-backoffice-ca/src/lib/error.ts
+++ apps/io-sign-backoffice-ca/src/lib/error.ts
@@ -6,20 +6,22 @@ export class HttpError extends Error {
   status = 500;
 }
 
-class HttpBadRequestError extends HttpError {
-  error = this.message;
-  status = 400;
-}
-
 class HttpConflictError extends HttpError {
   error = this.message;
   status = 409;
 }
 
+class HttpUnprocessableContentError extends HttpError {
+  error = this.message;
+  status = 422;
+}
+
 export const getHttpError: (e: Error) => HttpError = (e) => {
   switch (e.name) {
     case "ZodError":
-      return new HttpBadRequestError("error parsing the request body");
+      return new HttpUnprocessableContentError(
+        "error parsing the request body"
+      );
     case "ApiKeyAlreadyExistsError":
       return new HttpConflictError(e.message);
     default:
