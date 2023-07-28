d7682ac600be869893548d0d7b574315f7cf19a9
silvicir <127971996+silvicir@users.noreply.github.com>
2023-07-28 17:24:09 +0200

    updated controller

--- apps/io-sign-backoffice-ca/src/app/api/api-keys/route.ts
+++ apps/io-sign-backoffice-ca/src/app/api/api-keys/route.ts
@@ -1,16 +1,33 @@
 import { NextResponse, NextRequest } from "next/server";
-import { ApiKeyBody, createApiKey } from "./_lib/api-key";
-import { getHttpError } from "@/lib/error";
-
-const getResponseFromError = (e: Error) => {
-  const { error, status } = getHttpError(e);
-  return NextResponse.json({ error }, { status });
-};
+import {
+  ApiKeyAlreadyExistsError,
+  ApiKeyBody,
+  createApiKey,
+} from "./_lib/api-key";
+import { ZodError } from "zod";
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const parsedBody = ApiKeyBody.parse(body);
-  return createApiKey(parsedBody)
-    .then((body: unknown) => NextResponse.json(body, { status: 201 }))
-    .catch(getResponseFromError);
+  try {
+    const body = await request.json();
+    const parsedBody = ApiKeyBody.parse(body);
+    const res = await createApiKey(parsedBody);
+    return NextResponse.json(res, { status: 201 });
+  } catch (e) {
+    if (e instanceof ZodError) {
+      return NextResponse.json(
+        { error: "error parsing the request body" },
+        { status: 422 }
+      );
+    }
+    if (e instanceof ApiKeyAlreadyExistsError) {
+      return NextResponse.json({ error: e.message }, { status: 409 });
+    }
+    if (e instanceof Error) {
+      return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
