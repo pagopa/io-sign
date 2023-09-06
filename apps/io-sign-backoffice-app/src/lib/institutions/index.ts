c8ddc8f93dccceca1bb0004cc51bf1fc49a63960
silvicir <127971996+silvicir@users.noreply.github.com>
2023-09-06 17:05:25 +0200

    vatNumber = taxCode

--- apps/io-sign-backoffice-app/src/lib/institutions/index.ts
+++ apps/io-sign-backoffice-app/src/lib/institutions/index.ts
@@ -9,40 +9,17 @@ export const institutionSchema = z
     id: z.string().uuid(),
     description: z.string().nonempty(),
     taxCode: z.string().nonempty(),
-    onboarding: z
-      .object({
-        productId: z.string(),
-        status: z.string(),
-        billing: z.object({
-          vatNumber: z.string(),
-        }),
-      })
-      .array()
-      .min(1)
-      .refine((onboarding) =>
-        onboarding.some((e) => e.productId === "prod-io-sign")
-      ),
     userProductRoles: z.array(UserRole).default(["operator"]),
     logo: z.string().url(),
   })
-  .transform(
-    ({
-      id,
-      description: name,
-      taxCode,
-      onboarding,
-      userProductRoles,
-      logo,
-    }) => ({
-      id,
-      name,
-      taxCode,
-      vatNumber: onboarding.find((x) => x.productId === "prod-io-sign")!.billing
-        .vatNumber,
-      productRole: userProductRoles.at(0) ?? "Operatore",
-      logo,
-    })
-  );
+  .transform(({ id, description: name, taxCode, userProductRoles, logo }) => ({
+    id,
+    name,
+    taxCode,
+    vatNumber: taxCode,
+    productRole: userProductRoles.at(0) ?? "Operatore",
+    logo,
+  }));
 
 export type Institution = z.infer<typeof institutionSchema>;
 
