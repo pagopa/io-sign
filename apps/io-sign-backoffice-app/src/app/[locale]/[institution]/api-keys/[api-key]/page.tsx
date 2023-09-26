f92b6502cd350168377eb10623185c00195bfb53
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2023-09-26 16:42:01 +0200

    feat(bo): add info alert when the api key is empty

--- apps/io-sign-backoffice-app/src/app/[locale]/[institution]/api-keys/[api-key]/page.tsx
+++ apps/io-sign-backoffice-app/src/app/[locale]/[institution]/api-keys/[api-key]/page.tsx
@@ -12,6 +12,7 @@ import ApiKeyEditableFieldCard from "./_components/ApiKeyEditableFieldCard";
 import ApiKeyProvider from "./_components/ApiKeyProvider";
 import ApiKeySummary from "./_components/ApiKeySummary";
 import ApiKeyCreatedSnackbar from "./_components/ApiKeyCreatedSnackbar";
+import { Alert } from "@mui/material";
 
 export default function ApiKeyDetailPage({
   params,
@@ -19,10 +20,13 @@ export default function ApiKeyDetailPage({
   params: { institution: string; ["api-key"]: string };
 }) {
   const t = useTranslations("firmaconio");
+
   const apiKey = use(
     getApiKeyWithSecret(params["api-key"], params["institution"])
   );
+
   const apiKeysHref = `/${params.institution}/api-keys`;
+
   const header: PageProps["header"] = {
     title: apiKey.displayName,
     navigation: {
@@ -34,8 +38,18 @@ export default function ApiKeyDetailPage({
     },
   };
 
+  const showAlert =
+    apiKey.status === "active" &&
+    (apiKey.cidrs.length == 0 ||
+      (apiKey.environment === "test" && apiKey.testers.length === 0));
+
   return (
     <Page header={header}>
+      {showAlert && (
+        <Alert severity="info" variant="outlined">
+          {t(`apiKey.alerts.empty.${apiKey.environment}`)}
+        </Alert>
+      )}
       <ApiKeyProvider value={apiKey}>
         <Suspense>
           <ApiKeySummary apiKey={apiKey} />
