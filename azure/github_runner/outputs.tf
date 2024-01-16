3127f48cccb2e1935579efa61d497a86213bd3b2
Andrea Grillo <andrea.grillo@pagopa.it>
2024-01-16 12:49:19 +0100

    [EC-85] Move Azure config to io-infra (#247)

--- azure/github_runner/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ca_job_id" {
-  value       = module.container_app_job.id
-  description = "Container App job id"
-}
-
-output "ca_job_name" {
-  value       = module.container_app_job.name
-  description = "Container App job name"
-}
