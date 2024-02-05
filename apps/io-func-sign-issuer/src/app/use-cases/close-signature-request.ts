b63fc684ac8fe879adf30b690ace83811f2eeda8
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2024-02-05 14:30:02 +0100

    add close signature request use case

---
+++ apps/io-func-sign-issuer/src/app/use-cases/close-signature-request.ts
@@ -0,0 +1,23 @@
+import { pipe } from "fp-ts/lib/function";
+import * as RTE from "fp-ts/lib/ReaderTaskEither";
+
+import {
+  SignatureRequest,
+  getSignatureRequest,
+  upsertSignatureRequest,
+} from "../../signature-request";
+
+// TODO(): move here the signature request cancelation logic
+// since CANCELLED is an "end status" such as SIGNED and REJECTED
+// it should be treated in the same way, so:
+// - the user should be notified by a Message
+// - we should send a "CANCELLED" event to analytics
+
+// "closeSignatureRequest" ends the Signature Request lifecycle
+// A request is defined "CLOSED" when its status is one of: CANCELLED, SIGNED, REJECTED
+// Closed requests cannot be modified, whatever its outcome.
+export const closeSignatureRequest = (request: SignatureRequest) =>
+  pipe(
+    getSignatureRequest(request.id, request.issuerId),
+    RTE.chain(upsertSignatureRequest),
+  );
