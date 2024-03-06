04d9cb8c37688bf7ac85f2a860116898f56660d2
silvicir <127971996+silvicir@users.noreply.github.com>
2024-03-06 14:33:27 +0100

    updated /consents handler

--- apps/io-sign-backoffice-app/src/app/api/institutions/[institution]/consents/route.ts
+++ apps/io-sign-backoffice-app/src/app/api/institutions/[institution]/consents/route.ts
@@ -1,8 +1,13 @@
-import { z } from "zod";
+import { ZodError, z } from "zod";
 import { NextRequest, NextResponse } from "next/server";
 
-import { getLoggedUser } from "@/lib/auth/use-cases";
+import {
+  UnauthenticatedUserError,
+  getLoggedUser,
+  isAllowedInstitution,
+} from "@/lib/auth/use-cases";
 import { insertTOSAcceptance } from "@/lib/consents/cosmos";
+import { ValidationProblem } from "@/lib/api/responses";
 
 const pathSchema = z.object({
   institution: z.string().uuid(),
@@ -11,39 +16,42 @@ const pathSchema = z.object({
 type Params = z.infer<typeof pathSchema>;
 
 export async function POST(
-  request: NextRequest,
+  _request: NextRequest,
   { params }: { params: Params }
 ) {
   try {
-    pathSchema.parse(params);
-  } catch (e) {
-    return NextResponse.json(
-      {
-        title: "Bad request",
-        status: 400,
-      },
-      {
-        status: 400,
-        headers: {
-          "Content-Type": "application/problem+json",
-        },
-      }
-    );
-  }
-  try {
     const loggedUser = await getLoggedUser();
+    pathSchema.parse(params);
+    const isAllowedInstitutionId = await isAllowedInstitution(
+      loggedUser.id,
+      params.institution
+    );
+    if (!isAllowedInstitutionId) {
+      return NextResponse.json(
+        { title: "Forbidden", detail: "The operation is forbidden" },
+        { status: 403, headers: { "Content-Type": "application/problem+json" } }
+      );
+    }
     await insertTOSAcceptance(loggedUser.id, params.institution);
     return new Response(null, { status: 204 });
   } catch (e) {
+    if (e instanceof UnauthenticatedUserError) {
+      return NextResponse.json(
+        { title: "Unauthorized", detail: "Unauthorized to update consent" },
+        { status: 401, headers: { "Content-Type": "application/problem+json" } }
+      );
+    } else if (e instanceof ZodError) {
+      return NextResponse.json(ValidationProblem(e), {
+        status: 422,
+        headers: { "Content-Type": "application/problem+json" },
+      });
+    }
     return NextResponse.json(
       {
         title: "Internal Server Error",
         detail: e instanceof Error ? e.message : "Something went wrong.",
       },
-      {
-        status: 500,
-        headers: { "Content-Type": "application/problem+json" },
-      }
+      { status: 500, headers: { "Content-Type": "application/problem+json" } }
     );
   }
 }
