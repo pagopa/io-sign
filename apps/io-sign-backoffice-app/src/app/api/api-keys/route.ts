b353c84dc8da00cc9a66cca49373577a098e2e1d
silvicir <127971996+silvicir@users.noreply.github.com>
2024-03-06 14:45:36 +0100

    updated /api-keys handler

--- apps/io-sign-backoffice-app/src/app/api/api-keys/route.ts
+++ apps/io-sign-backoffice-app/src/app/api/api-keys/route.ts
@@ -7,48 +7,41 @@ import {
   ApiKeyAlreadyExistsError,
 } from "@/lib/api-keys/use-cases";
 import { ValidationProblem } from "@/lib/api/responses";
-import { UnauthenticatedUserError, getLoggedUser } from "@/lib/auth/use-cases";
+import {
+  UnauthenticatedUserError,
+  getLoggedUser,
+  isAllowedInstitution,
+} from "@/lib/auth/use-cases";
 
 export async function POST(request: NextRequest) {
   try {
-    await getLoggedUser();
-  } catch (e) {
-    if (e instanceof UnauthenticatedUserError) {
-      return NextResponse.json(
-        {
-          title: "Unauthorized",
-          detail: e.message,
-        },
-        {
-          status: 401,
-          headers: { "Content-Type": "application/problem+json" },
-        }
-      );
-    }
-    return NextResponse.json(
-      {
-        title: "Internal Server Error",
-        detail: e instanceof Error ? e.message : "Something went wrong.",
-      },
-      {
-        status: 500,
-        headers: { "Content-Type": "application/problem+json" },
-      }
-    );
-  }
-  try {
+    const loggedUser = await getLoggedUser();
     const body = await request.json();
     const parsedBody = createApiKeyPayloadSchema.parse(body);
+    const isAllowedInstitutionId = await isAllowedInstitution(
+      loggedUser.id,
+      parsedBody.institutionId
+    );
+    if (!isAllowedInstitutionId) {
+      return NextResponse.json(
+        { title: "Forbidden", detail: "The operation is forbidden" },
+        { status: 403, headers: { "Content-Type": "application/problem+json" } }
+      );
+    }
     const apiKeyId = await createApiKey(parsedBody);
     return NextResponse.json({ id: apiKeyId }, { status: 201 });
   } catch (e) {
-    if (e instanceof ZodError) {
+    if (e instanceof UnauthenticatedUserError) {
+      return NextResponse.json(
+        { title: "Unauthorized", detail: "Unauthorized to create the API key" },
+        { status: 401, headers: { "Content-Type": "application/problem+json" } }
+      );
+    } else if (e instanceof ZodError) {
       return NextResponse.json(ValidationProblem(e), {
         status: 422,
         headers: { "Content-Type": "application/problem+json" },
       });
-    }
-    if (e instanceof ApiKeyAlreadyExistsError) {
+    } else if (e instanceof ApiKeyAlreadyExistsError) {
       return NextResponse.json(
         { title: "Conflict Error", detail: e.message },
         { status: 409, headers: { "Content-Type": "application/problem+json" } }
