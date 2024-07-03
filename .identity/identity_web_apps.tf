5aa5f760ab06c0128565c7b2121e88dd9aa4d4c1
Mario Mupo <mario.mupo@pagopa.it>
2024-07-03 10:38:30 +0200

    moved identities

--- .identity/identity_web_apps.tf
+++ .identity/identity_web_apps.tf
@@ -1,18 +1,18 @@
-module "web_apps_identity_cd" {
-  source = "github.com/pagopa/terraform-azurerm-v3//github_federated_identity?ref=v7.34.2"
+# module "web_apps_identity_cd" {
+#   source = "github.com/pagopa/terraform-azurerm-v3//github_federated_identity?ref=v7.34.2"
 
-  prefix    = var.prefix
-  env_short = var.env_short
-  domain    = var.domain
+#   prefix    = var.prefix
+#   env_short = var.env_short
+#   domain    = var.domain
 
-  identity_role = "cd"
+#   identity_role = "cd"
 
-  github_federations = local.github_federations
+#   github_federations = local.github_federations
 
-  cd_rbac_roles = {
-    subscription_roles = var.web_apps_environment_cd_roles.subscription
-    resource_groups    = var.web_apps_environment_cd_roles.resource_groups
-  }
+#   cd_rbac_roles = {
+#     subscription_roles = var.web_apps_environment_cd_roles.subscription
+#     resource_groups    = var.web_apps_environment_cd_roles.resource_groups
+#   }
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
--- infra/identity/prod/main.tf
+++ infra/identity/prod/main.tf
@@ -25,28 +25,50 @@ module "federated_identities" {
   prefix    = local.prefix
   env_short = local.env_short
   env       = local.env
-  domain    = "${local.domain}-infra"
+  domain    = "${local.domain}"
 
   repositories = [local.repo_name]
 
+  tags = local.tags
+}
+
+module "federated_identities_web_apps" {
+  source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=main"
+
+  prefix    = local.prefix
+  env_short = local.env_short
+  env       = "app-${local.env}"
+  domain    = "${local.domain}-app"
+
+  repositories = [local.repo_name]
+  continuos_integration = {
+    enable = false
+  }
+
+  tags = local.tags
+}
+
+module "federated_identities_opex" {
+  source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=main"
+
+  prefix    = local.prefix
+  env_short = local.env_short
+  env       = "opex-${local.env}"
+  domain    = "${local.domain}-opex"
+
+  repositories = [local.repo_name]
   continuos_integration = {
     enable = true
+
     roles = {
-      subscription = [
-        "Reader",
-        "PagoPA IaC Reader",
-        "Reader and Data Access",
-        "Storage Blob Data Reader",
-        "Storage File Data SMB Share Reader",
-        "Storage Queue Data Reader",
-        "Storage Table Data Reader",
-        "Key Vault Reader",
-        "DocumentDB Account Contributor",
-        "API Management Service Contributor",
-      ]
+      subscription = []
       resource_groups = {
+        dashboards = [
+          "Reader"
+        ]
         terraform-state-rg = [
-          "Storage Blob Data Contributor"
+          "Storage Blob Data Reader",
+          "Reader and Data Access"
         ]
       }
     }
@@ -54,17 +76,18 @@ module "federated_identities" {
 
   continuos_delivery = {
     enable = true
+
     roles = {
-      subscription = [
-        "Contributor",
-        "Storage Account Contributor",
-        "Storage Blob Data Contributor",
-        "Storage File Data SMB Share Contributor",
-        "Storage Queue Data Contributor",
-        "Storage Table Data Contributor",
-        "Key Vault Contributor",
-      ]
-      resource_groups = {}
+      subscription = []
+      resource_groups = {
+        dashboards = [
+          "Contributor"
+        ]
+        terraform-state-rg = [
+          "Storage Blob Data Contributor",
+          "Reader and Data Access"
+        ]
+      }
     }
   }
 
