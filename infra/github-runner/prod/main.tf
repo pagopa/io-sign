898ffad39e900b900fc34e4042ccf253d60b9e3e
Andrea Grillo <andrea.grillo@pagopa.it>
2024-07-03 10:24:01 +0200

    update state file name

--- infra/github-runner/prod/main.tf
+++ infra/github-runner/prod/main.tf
@@ -10,7 +10,7 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfappprodio"
     container_name       = "terraform-state"
-    key                  = "io-fims.github-runner.tfstate"
+    key                  = "io-sign.github-runner.tfstate"
   }
 }
 
