6bb3b71bad4cd5c7e724b69b0a4eae0106bae5c7
Mario Mupo <mario.mupo@pagopa.it>
2024-07-24 12:45:15 +0200

    fix: edited environment secrets and variables configuration for repository io-sign

--- infra/repository/github_environment_cd.tf
+++ infra/repository/github_environment_cd.tf
@@ -24,3 +24,80 @@ resource "github_actions_environment_secret" "env_prod_cd_secrets" {
   secret_name     = each.key
   plaintext_value = each.value
 }
+
+# -------------- OPEX ----------------
+
+resource "github_repository_environment" "github_repository_environment_opex_prod_cd" {
+  environment = "opex-prod-cd"
+  repository  = github_repository.this.name
+
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = true
+  }
+
+  # reviewers {
+  #   teams = matchkeys(
+  #     data.github_organization_teams.all.teams[*].id,
+  #     data.github_organization_teams.all.teams[*].slug,
+  #     local.cd_opex.reviewers_teams
+  #   )
+  # }
+}
+
+resource "github_actions_environment_secret" "env_opex_prod_cd_secrets" {
+  for_each = local.cd_opex.secrets
+
+  repository      = github_repository.this.name
+  environment     = github_repository_environment.github_repository_environment_opex_prod_cd.environment
+  secret_name     = each.key
+  plaintext_value = each.value
+}
+
+# -------------- WEB-APP ----------------
+
+resource "github_repository_environment" "web_apps" {
+  for_each    = local.web_apps_map
+  environment = each.value.name
+  repository  = github_repository.this.name
+
+  reviewers {
+    teams = matchkeys(
+      data.github_organization_teams.all.teams[*].id,
+      data.github_organization_teams.all.teams[*].slug,
+      local.cd_web_apps.reviewers_teams
+    )
+  }
+}
+
+resource "github_actions_environment_secret" "web_app_client_id" {
+  for_each        = local.web_apps_map
+  repository      = github_repository.this.name
+  environment     = github_repository_environment.web_apps[each.key].environment
+  secret_name     = "AZURE_CLIENT_ID"
+  plaintext_value = local.cd_web_apps.secrets["AZURE_CLIENT_ID"]
+}
+
+resource "github_actions_environment_variable" "web_app_resouce_group" {
+  for_each      = local.web_apps_map
+  repository    = github_repository.this.name
+  environment   = github_repository_environment.web_apps[each.key].environment
+  variable_name = "AZURE_WEB_APP_RESOURCE_GROUP"
+  value         = local.backend_resource_group_name
+}
+
+resource "github_actions_environment_variable" "web_app_names" {
+  for_each      = local.web_apps_map
+  repository    = github_repository.this.name
+  environment   = github_repository_environment.web_apps[each.key].environment
+  variable_name = "AZURE_WEB_APP_NAME"
+  value         = each.value.name
+}
+
+resource "github_actions_environment_variable" "health_check_path" {
+  for_each      = local.web_apps_map
+  repository    = github_repository.this.name
+  environment   = github_repository_environment.web_apps[each.key].environment
+  variable_name = "HEALTH_CHECK_PATH"
+  value         = coalesce(data.azurerm_linux_web_app.web_apps[each.key].site_config[0].health_check_path, "/")
+}
--- infra/repository/locals.tf
+++ infra/repository/locals.tf
@@ -2,22 +2,46 @@ locals {
   project = "io-p"
 
   identity_resource_group_name = "${local.project}-identity-rg"
+  backend_resource_group_name = "${local.project}-sign-backend-rg"
 
   repo_secrets = {
-    "ARM_TENANT_ID"       = data.azurerm_client_config.current.tenant_id,
-    "ARM_SUBSCRIPTION_ID" = data.azurerm_subscription.current.subscription_id
+    "AZURE_TENANT_ID"       = data.azurerm_client_config.current.tenant_id,
+    "AZURE_SUBSCRIPTION_ID" = data.azurerm_subscription.current.subscription_id
   }
 
   ci = {
     secrets = {
-      "ARM_CLIENT_ID" = data.azurerm_user_assigned_identity.identity_prod_ci.client_id
+      "AZURE_CLIENT_ID" = data.azurerm_user_assigned_identity.identity_prod_ci.client_id
     }
   }
 
   cd = {
     secrets = {
-      "ARM_CLIENT_ID" = data.azurerm_user_assigned_identity.identity_prod_cd.client_id
+      "AZURE_CLIENT_ID" = data.azurerm_user_assigned_identity.identity_prod_cd.client_id
     }
     reviewers_teams = ["io-backend-contributors", "io-backend-admin", "engineering-team-cloud-eng"]
+  }
+
+  # OPEX
+  ci_opex = {
+    secrets = {
+      "AZURE_CLIENT_ID" = data.azurerm_user_assigned_identity.identity_opex_prod_ci.client_id
+    }
+  }
+
+  cd_opex = {
+    secrets = {
+      "AZURE_CLIENT_ID" = data.azurerm_user_assigned_identity.identity_opex_prod_cd.client_id
+    }
+  }
+
+  # WEB-APP
+  web_apps_map = { for w in data.azurerm_resources.web_apps.resources : w.name => w }
+
+  cd_web_apps = {
+    secrets = {
+      "AZURE_CLIENT_ID" = data.azurerm_user_assigned_identity.identity_app_prod_cd.client_id
+    }
+    reviewers_teams = ["io-backend-contributors", "io-backend-admin", "engineering-team-cloud-eng","io-sign-maintainers"]
   }
 }
