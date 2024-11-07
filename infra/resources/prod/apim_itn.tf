081452ff3f29fc7c51114d96c8003702c4164e5a
Mario Mupo <mario.mupo@pagopa.it>
2024-11-07 15:21:42 +0100

    feat: added all apim resources

--- infra/resources/prod/apim_itn.tf
+++ infra/resources/prod/apim_itn.tf
@@ -3,103 +3,103 @@ data "azurerm_api_management" "apim_itn_api" {
   resource_group_name = "${local.project_itn}-common-rg-01"
 }
 
-# resource "azurerm_api_management_named_value" "io_fn_sign_issuer_url_itn" {
-#   name                = "io-fn-sign-issuer-url"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-fn-sign-issuer-url"
-#   value               = format("https://%s-sign-issuer-func.azurewebsites.net", local.product)
-# }
+resource "azurerm_api_management_named_value" "io_fn_sign_issuer_url_itn" {
+  name                = "io-fn-sign-issuer-url"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-fn-sign-issuer-url"
+  value               = format("https://%s-sign-issuer-func.azurewebsites.net", local.product)
+}
 
-# resource "azurerm_api_management_named_value" "io_fn_sign_issuer_key_itn" {
-#   name                = "io-fn-sign-issuer-key"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-fn-sign-issuer-key"
-#   value               = module.key_vault_secrets.values["io-fn-sign-issuer-key"].value
-#   secret              = true
-# }
+resource "azurerm_api_management_named_value" "io_fn_sign_issuer_key_itn" {
+  name                = "io-fn-sign-issuer-key"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-fn-sign-issuer-key"
+  value               = module.key_vault_secrets.values["io-fn-sign-issuer-key"].value
+  secret              = true
+}
 
-# resource "azurerm_api_management_named_value" "io_fn_sign_support_url_itn" {
-#   name                = "io-fn-sign-support-url"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-fn-sign-support-url"
-#   value               = format("https://%s-sign-support-func.azurewebsites.net", local.product)
-# }
+resource "azurerm_api_management_named_value" "io_fn_sign_support_url_itn" {
+  name                = "io-fn-sign-support-url"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-fn-sign-support-url"
+  value               = format("https://%s-sign-support-func.azurewebsites.net", local.product)
+}
 
-# resource "azurerm_api_management_named_value" "io_fn_sign_support_key_itn" {
-#   name                = "io-fn-sign-support-key"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-fn-sign-support-key"
-#   value               = module.key_vault_secrets.values["io-fn-sign-support-key"].value
-#   secret              = true
-# }
+resource "azurerm_api_management_named_value" "io_fn_sign_support_key_itn" {
+  name                = "io-fn-sign-support-key"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-fn-sign-support-key"
+  value               = module.key_vault_secrets.values["io-fn-sign-support-key"].value
+  secret              = true
+}
 
 
-# resource "azurerm_api_management_named_value" "io_sign_cosmosdb_name_itn" {
-#   name                = "io-sign-cosmosdb-name"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-sign-cosmosdb-name"
-#   value               = module.cosmosdb_account.name
-#   secret              = false
-# }
+resource "azurerm_api_management_named_value" "io_sign_cosmosdb_name_itn" {
+  name                = "io-sign-cosmosdb-name"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-sign-cosmosdb-name"
+  value               = module.cosmosdb_account.name
+  secret              = false
+}
 
-# resource "azurerm_api_management_named_value" "io_sign_cosmosdb_key_itn" {
-#   name                = "io-sign-cosmosdb-key"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-sign-cosmosdb-key"
-#   value               = module.cosmosdb_account.primary_readonly_key
-#   secret              = true
-# }
+resource "azurerm_api_management_named_value" "io_sign_cosmosdb_key_itn" {
+  name                = "io-sign-cosmosdb-key"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-sign-cosmosdb-key"
+  value               = module.cosmosdb_account.primary_readonly_key
+  secret              = true
+}
 
-# # legacy, it can be removed once the backoffice is released
-# resource "azurerm_api_management_named_value" "io_sign_cosmosdb_issuer_container_name_itn" {
-#   name                = "io-sign-cosmosdb-issuer-container-name"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-sign-cosmosdb-issuer-container-name"
-#   value               = module.cosmosdb_sql_database_issuer.name
-#   secret              = false
-# }
-# resource "azurerm_api_management_named_value" "io_sign_cosmosdb_issuer_whitelist_collection_name_new_itn" {
-#   name                = "io-sign-cosmosdb-issuer-whitelist-collection-name"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-sign-cosmosdb-issuer-whitelist-collection-name"
-#   value               = module.cosmosdb_sql_container_issuer-issuers-whitelist.name
-#   secret              = false
-# }
-# resource "azurerm_api_management_named_value" "io_sign_cosmosdb_issuer_issuers_collection_name_itn" {
-#   name                = "io-sign-cosmosdb-issuer-issuers-name"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-sign-cosmosdb-issuer-issuers-name"
-#   value               = module.cosmosdb_sql_container_issuer-issuers.name
-#   secret              = false
-# }
-# # end legacy
+# legacy, it can be removed once the backoffice is released
+resource "azurerm_api_management_named_value" "io_sign_cosmosdb_issuer_container_name_itn" {
+  name                = "io-sign-cosmosdb-issuer-container-name"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-sign-cosmosdb-issuer-container-name"
+  value               = module.cosmosdb_sql_database_issuer.name
+  secret              = false
+}
+resource "azurerm_api_management_named_value" "io_sign_cosmosdb_issuer_whitelist_collection_name_new_itn" {
+  name                = "io-sign-cosmosdb-issuer-whitelist-collection-name"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-sign-cosmosdb-issuer-whitelist-collection-name"
+  value               = module.cosmosdb_sql_container_issuer-issuers-whitelist.name
+  secret              = false
+}
+resource "azurerm_api_management_named_value" "io_sign_cosmosdb_issuer_issuers_collection_name_itn" {
+  name                = "io-sign-cosmosdb-issuer-issuers-name"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-sign-cosmosdb-issuer-issuers-name"
+  value               = module.cosmosdb_sql_container_issuer-issuers.name
+  secret              = false
+}
+# end legacy
 
-# resource "azurerm_api_management_named_value" "backoffice-database-name" {
-#   name                = "io-sign-backoffice-database-name"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-sign-backoffice-database-name"
-#   value               = module.cosmosdb_sql_database_backoffice.name
-#   secret              = false
-# }
+resource "azurerm_api_management_named_value" "backoffice-database-name_itn" {
+  name                = "io-sign-backoffice-database-name"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-sign-backoffice-database-name"
+  value               = module.cosmosdb_sql_database_backoffice.name
+  secret              = false
+}
 
-# resource "azurerm_api_management_named_value" "backoffice-api-keys-collection-name" {
-#   name                = "io-sign-backoffice-api-keys-collection-name"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-sign-backoffice-api-keys-collection-name"
-#   value               = module.cosmosdb_sql_container_backoffice-api-keys.name
-#   secret              = false
-# }
+resource "azurerm_api_management_named_value" "backoffice-api-keys-collection-name_itn" {
+  name                = "io-sign-backoffice-api-keys-collection-name"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-sign-backoffice-api-keys-collection-name"
+  value               = module.cosmosdb_sql_container_backoffice-api-keys.name
+  secret              = false
+}
 
 module "apim_itn_io_sign_product" {
   source = "github.com/pagopa/terraform-azurerm-v3//api_management_product?ref=v8.35.0"
@@ -118,14 +118,14 @@ module "apim_itn_io_sign_product" {
   policy_xml = file("./api_product/sign/_base_policy.xml")
 }
 
-# resource "azurerm_api_management_api_operation_policy" "get_signer_by_fiscal_code_policy_itn" {
-#   api_name            = module.apim_itn_io_sign_issuer_api_v1.name
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   operation_id        = "getSignerByFiscalCode"
+resource "azurerm_api_management_api_operation_policy" "get_signer_by_fiscal_code_policy_itn" {
+  api_name            = module.apim_itn_io_sign_issuer_api_v1.name
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  operation_id        = "getSignerByFiscalCode"
 
-#   xml_content = file("./api/issuer/v1/get_signer_by_fiscal_code_policy/policy.xml")
-# }
+  xml_content = file("./api/issuer/v1/get_signer_by_fiscal_code_policy/policy.xml")
+}
 
 module "apim_itn_io_sign_issuer_api_v1" {
   source = "github.com/pagopa/terraform-azurerm-v3//api_management_api?ref=v8.35.0"
@@ -190,22 +190,22 @@ module "apim_itn_io_sign_support_api_v1" {
 
 # BACK OFFICE
 
-# resource "azurerm_api_management_named_value" "io_fn_sign_backoffice_url_itn" {
-#   name                = "io-fn-sign-backoffice-url"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-fn-sign-backoffice-url"
-#   value               = format("https://%s-sign-backoffice-func.azurewebsites.net", local.product)
-# }
+resource "azurerm_api_management_named_value" "io_fn_sign_backoffice_url_itn" {
+  name                = "io-fn-sign-backoffice-url"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-fn-sign-backoffice-url"
+  value               = format("https://%s-sign-backoffice-func.azurewebsites.net", local.product)
+}
 
-# resource "azurerm_api_management_named_value" "io_fn_sign_backoffice_key_itn" {
-#   name                = "io-fn-sign-backoffice-key"
-#   api_management_name = data.azurerm_api_management.apim_itn_api.name
-#   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-#   display_name        = "io-fn-sign-backoffice-key"
-#   value               = module.key_vault_secrets.values["io-sign-backoffice-func-key"].value
-#   secret              = true
-# }
+resource "azurerm_api_management_named_value" "io_fn_sign_backoffice_key_itn" {
+  name                = "io-fn-sign-backoffice-key"
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  display_name        = "io-fn-sign-backoffice-key"
+  value               = module.key_vault_secrets.values["io-sign-backoffice-func-key"].value
+  secret              = true
+}
 
 module "apim_itn_io_sign_backoffice_product" {
   source = "github.com/pagopa/terraform-azurerm-v3//api_management_product?ref=v8.35.0"
