ac6e01eeba7570f2ccc0d443d6a0e02e0d34fbce
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-08 12:00:08 +0100

    CES-465-migrate-iopweucitizenauthst delete unused code

--- infra/resources/prod/locals.tf
+++ infra/resources/prod/locals.tf
@@ -4,17 +4,17 @@ locals {
   project_itn = "${local.product}-itn"
 }
 
-###Italy North
-locals {
-  prefix = "io"
-  env_short = "p"
-  domain = "citizenauth"
-  instance_number = "01"
-  itn_environment = {
-    prefix          = local.prefix
-    env_short       = local.env_short
-    location        = var.location
-    domain          = local.domain
-    instance_number = local.instance_number
-    }
-}
+# ###Italy North
+# locals {
+#   prefix = "io"
+#   env_short = "p"
+#   domain = ""
+#   instance_number = "01"
+#   itn_environment = {
+#     prefix          = local.prefix
+#     env_short       = local.env_short
+#     location        = var.location
+#     domain          = local.domain
+#     instance_number = local.instance_number
+#     }
+# }
