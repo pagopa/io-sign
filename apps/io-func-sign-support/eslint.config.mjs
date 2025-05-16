3bd0d6efed63837f05f792b74484443e5c0ad703
Roberto Cocco <roberto.cocco@it.ey.com>
2025-05-16 12:13:01 +0200

    Add io-sign/eslint-config package

--- apps/io-func-sign-support/eslint.config.mjs
+++ apps/io-func-sign-support/eslint.config.mjs
@@ -1,31 +1,5 @@
-import pagopa from "@pagopa/eslint-config";
+import ioSign from "@io-sign/eslint-config";
 
 export default [
-  ...pagopa,
-  {
-    rules: {
-      "comma-dangle": "off",
-      "perfectionist/sort-classes": "off",
-      "perfectionist/sort-enums": "off",
-      "perfectionist/sort-imports": "off",
-      "perfectionist/sort-interfaces": "off",
-      "perfectionist/sort-intersection-types": "off",
-      "perfectionist/sort-objects": "off",
-      "perfectionist/sort-object-types": "off",
-      "perfectionist/sort-union-types": "off",
-      "@typescript-eslint/array-type": ["error", { default: "generic" }],
-      "@typescript-eslint/no-inferrable-types": "off"
-    }
-  },
-  {
-    ignores: [
-      "node_modules",
-      "dist",
-      "**/*.spec.ts",
-      "*.yaml",
-      "**/models/*.ts",
-      "*.pdf",
-      "*.json"
-    ]
-  }
+  ...ioSign,
 ];
