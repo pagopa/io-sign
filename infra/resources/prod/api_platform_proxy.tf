b4ceb735a9883d4421bc2dee3e9fc4fdac1f924e
gquadrati <greta.quadrati@pagopa.it>
2026-01-15 17:13:11 +0100

    fix: use domain kv

--- infra/resources/prod/api_platform_proxy.tf
+++ infra/resources/prod/api_platform_proxy.tf
@@ -3,10 +3,6 @@ data "azurerm_api_management" "platform_apim" {
   name                = "${local.project_itn}-platform-api-gateway-apim-01"
 }
 
-data "azurerm_key_vault" "legacy_common_kv" {
-  name                = "${local.project_itn}-kv-common"
-  resource_group_name = "${local.project_itn}-rg-common"
-}
 
 module "io_platform_apim_api" {
   source = "../modules/platform_proxy_api"
@@ -15,5 +11,5 @@ module "io_platform_apim_api" {
   platform_apim_resource_group_name = data.azurerm_api_management.platform_apim.resource_group_name
   platform_apim_id                  = data.azurerm_api_management.platform_apim.id
 
-  key_vault_common_id = data.azurerm_key_vault.legacy_common_kv.id
+  key_vault_common_id = module.key_vault_itn.id
 }
