56a84a9eec0a59566c56fe6cb7b2604287776e8c
Mirco Civolani <mirco.civolani@pagopa.it>
2026-02-05 14:32:32 +0100

    Converting io-func-sign-support from azure programming model v3 to v4

--- apps/io-func-sign-support/src/infra/azure/functions/get-signature-request.ts
+++
@@ -1,7 +0,0 @@
-import { httpAzureFunction } from "@pagopa/handler-kit-azure-func";
-
-import { GetSignatureRequestHandler } from "../../http/handlers/get-signature-request";
-
-export const GetSignatureRequestFunction = httpAzureFunction(
-  GetSignatureRequestHandler
-);
