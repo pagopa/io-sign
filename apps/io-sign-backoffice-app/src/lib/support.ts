70670d0f070b073bf13de824d47431ef83c50a67
Stefano D'Aversa <stefano.daversa@pagopa.it>
2026-03-04 15:30:09 +0100

    change l3 support email

--- apps/io-sign-backoffice-app/src/lib/support.ts
+++ apps/io-sign-backoffice-app/src/lib/support.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 
 function getSupportContactsFromEnvironment() {
   const schema = z.object({
-    SUPPORT_L3_EMAIL: z.string().email().catch("firmaconio-tech@pagopa.it"),
+    SUPPORT_L3_EMAIL: z.string().email().catch("io-service-management@pagopa.it"),
   });
   return schema
     .transform(({ SUPPORT_L3_EMAIL }) => ({
