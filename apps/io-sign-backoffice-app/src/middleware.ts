cfc4fc959dcb9094e7703bf6de6403c3a999e88b
giuseppedipinto <giuseppeantonio.dipinto@pagopa.it>
2026-03-13 15:28:34 +0100

    remove default locale prefix from URLs

--- apps/io-sign-backoffice-app/src/middleware.ts
+++ apps/io-sign-backoffice-app/src/middleware.ts
@@ -3,6 +3,7 @@ import createIntlMiddleware from "next-intl/middleware";
 export default createIntlMiddleware({
   locales: ["it"],
   defaultLocale: "it",
+  localePrefix: "as-needed",
 });
 
 export const config = {
