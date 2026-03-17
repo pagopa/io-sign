edf6ac7c39c129a74d9a1512324ff7b5452d05b5
Iustin <iustinburlacu@gmail.com>
2026-03-17 10:38:42 +0100

    mapping cosmos db to many error to custom error

--- packages/io-sign/src/infra/azure/cosmos/errors.ts
+++ packages/io-sign/src/infra/azure/cosmos/errors.ts
@@ -1,5 +1,7 @@
 import { CosmosErrors } from "@pagopa/io-functions-commons/dist/src/utils/cosmosdb_model";
 
+import { TooManyRequestsError } from "../../../error";
+
 class CosmosDatabaseError extends Error {
   constructor(message: string) {
     super(message);
@@ -10,7 +12,14 @@ class CosmosDatabaseError extends Error {
 export const toCosmosDatabaseError = (e: CosmosErrors) => {
   switch (e.kind) {
     case "COSMOS_ERROR_RESPONSE":
-      return new CosmosDatabaseError(e.error.message);
+      // eslint-disable-next-line no-console
+      console.error(
+        `Cosmos DB error [code=${e.error.code}]: ${e.error.message}`
+      );
+      if (e.error.code === 429) {
+        return new TooManyRequestsError("Too many requests.");
+      }
+      return new CosmosDatabaseError("A database error has occurred.");
     case "COSMOS_CONFLICT_RESPONSE":
       return new CosmosDatabaseError("Conflict.");
     case "COSMOS_EMPTY_RESPONSE":
@@ -18,5 +27,9 @@ export const toCosmosDatabaseError = (e: CosmosErrors) => {
     case "COSMOS_DECODING_ERROR":
       // TODO: show details about validation
       return new CosmosDatabaseError("Decoding error.");
+    default:
+      // eslint-disable-next-line no-console
+      console.error("Cosmos DB unexpected error", JSON.stringify(e));
+      return new CosmosDatabaseError("Generic error.");
   }
 };
