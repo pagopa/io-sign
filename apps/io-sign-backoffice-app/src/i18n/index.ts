8681ba6e708357a771db1b074aa17e48ac2a2e17
giuseppedipinto <giuseppeantonio.dipinto@pagopa.it>
2026-03-17 15:46:05 +0100

    fix: add "it" fallback to requestLocale i18n config

--- apps/io-sign-backoffice-app/src/i18n/index.ts
+++ apps/io-sign-backoffice-app/src/i18n/index.ts
@@ -3,7 +3,7 @@ import { getRequestConfig } from "next-intl/server";
 import { defaultTranslationValues } from "./options";
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  const locale = await requestLocale;
+  const locale = (await requestLocale) ?? "it";
   return {
     locale,
     messages: (await import(`./messages/${locale}.json`)).default,
