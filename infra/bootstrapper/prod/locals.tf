b6dcca68985169c369fcf4640fb9b7aba5453df4
Stefano D'Aversa <stefano.daversa@pagopa.it>
2026-03-26 15:18:40 +0100

    feat : add bootstrapper (#477)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    resolve IEL-33
    #### List of Changes
    
    <!--- Describe your changes in detail -->
    
    #### Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    <!--- Attach screenshots in case changes impact UI. -->
    
    #### Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    
    ---------
    
    Co-authored-by: Christian Calabrese <christian.calabrese@pagopa.it>

---
+++ infra/bootstrapper/prod/locals.tf
@@ -0,0 +1,68 @@
+locals {
+  prefix          = "io"
+  env_short       = "p"
+  location        = "italynorth"
+  domain          = "sign"
+  instance_number = "01"
+  weu_prefix      = "${local.prefix}-${local.env_short}"
+  itn_prefix      = "${local.prefix}-${local.env_short}-itn"
+
+  adgroups = {
+    admins_name = "io-p-adgroup-ecosystem-n-links-admins"
+    devs_name   = "io-p-adgroup-ecosystem-n-links-developers"
+  }
+
+  runner = {
+    cae_name                = "${local.itn_prefix}-github-runner-cae-01"
+    cae_resource_group_name = "${local.itn_prefix}-github-runner-rg-01"
+    secret = {
+      kv_name                = "${local.itn_prefix}-common-kv-01"
+      kv_resource_group_name = "${local.itn_prefix}-common-rg-01"
+    }
+  }
+
+  apim = {
+    name                = "${local.itn_prefix}-apim-01"
+    resource_group_name = "${local.itn_prefix}-common-rg-01"
+  }
+
+  vnet = {
+    name                = "${local.itn_prefix}-common-vnet-01"
+    resource_group_name = "${local.itn_prefix}-common-rg-01"
+  }
+
+  dns_zones = {
+    resource_group_name = "${local.weu_prefix}-rg-common"
+  }
+
+  functions = {
+    itn_io_sign_rg_name             = "${local.itn_prefix}-sign-rg-01"
+    itn_io_sign_backend_rg_name     = "${local.itn_prefix}-sign-backend-rg-01"
+    itn_io_sign_integration_rg_name = "${local.itn_prefix}-sign-integration-rg-01"
+    itn_io_sign_data_rg_name        = "${local.itn_prefix}-sign-data-rg-01"
+    io_p_sign_backend_rg_name       = "${local.weu_prefix}-sign-backend-rg"
+  }
+
+  tf_storage_account = {
+    name                = "iopitntfst001"
+    resource_group_name = "terraform-state-rg"
+  }
+
+  repository = {
+    name = "io-sign"
+  }
+
+  key_vault = {
+    name                = "io-p-kv-common"
+    resource_group_name = "io-p-rg-common"
+  }
+
+  tags = {
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    BusinessUnit   = "App IO"
+    ManagementTeam = "IO Ecosystem Links"
+    CostCenter     = "TS000 - Tecnologia e Servizi"
+    Source         = "https://github.com/pagopa/io-sign/blob/main/infra/bootstrapper/prod"
+  }
+}
