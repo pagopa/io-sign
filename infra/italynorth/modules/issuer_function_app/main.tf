54036854fee0224d98b29873f89e17dfedbd625c
Stefano D'Aversa <stefano.daversa@pagopa.it>
2026-03-27 14:54:18 +0100

    create fn issuer in itn with dx modules

---
+++ infra/italynorth/modules/issuer_function_app/main.tf
@@ -0,0 +1,90 @@
+module "function_sign_issuer" {
+  source  = "pagopa-dx/azure-function-app/azurerm"
+  version = "~> 4.0"
+
+  environment = {
+    prefix          = local.prefix
+    env_short       = local.env_short
+    location        = local.location_itn
+    app_name        = "${local.domain}-issuer"
+    instance_number = local.instance_number
+  }
+
+  resource_group_name = data.azurerm_resource_group.sign_itn_backend_rg.name
+  size                = "P0v3"
+  node_version        = "20"
+
+  virtual_network = {
+    name                = var.vnet_common_name_itn
+    resource_group_name = var.common_resource_group_name_itn
+  }
+
+  subnet_cidr                          = var.sign_issuer_snet_cidr
+  health_check_path                    = "/api/v1/sign/info"
+  subnet_pep_id                        = data.azurerm_subnet.private_endpoints_subnet_itn.id
+  private_dns_zone_resource_group_name = data.azurerm_resource_group.weu-common.name
+
+  app_settings = merge(
+    local.io_sign_issuer_func.app_settings,
+    {
+      "AzureWebJobs.closeSignatureRequestRejected.Disabled" = "1"
+      "AzureWebJobs.closeSignatureRequestSigned.Disabled"   = "1"
+      "AzureWebJobs.CreateIssuerByVatNumberView.Disabled"   = "1"
+      "AzureWebJobs.MarkAsWaitForSignature.Disabled"        = "1"
+      "AzureWebJobs.ValidateUpload.Disabled"                = "1"
+    }
+  )
+
+  slot_app_settings = merge(
+    local.io_sign_issuer_func.app_settings,
+    {
+      "AzureWebJobs.closeSignatureRequestRejected.Disabled" = "1"
+      "AzureWebJobs.closeSignatureRequestSigned.Disabled"   = "1"
+      "AzureWebJobs.CreateIssuerByVatNumberView.Disabled"   = "1"
+      "AzureWebJobs.MarkAsWaitForSignature.Disabled"        = "1"
+      "AzureWebJobs.ValidateUpload.Disabled"                = "1"
+    }
+  )
+
+  action_group_ids = [data.azurerm_monitor_action_group.common_error_action_group.id, data.azurerm_monitor_action_group.sign_error_action_group.id]
+
+  tags = var.tags
+}
+
+module "itn_sign_issuer_func_roles" {
+  source          = "pagopa-dx/azure-role-assignments/azurerm"
+  version         = "~> 1.2.0"
+  principal_id    = module.function_sign_issuer.function_app.function_app.principal_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
+
+  key_vault = [
+    {
+      name                = data.azurerm_key_vault.sign_weu_kv.name
+      resource_group_name = data.azurerm_key_vault.sign_weu_kv.resource_group_name
+      description         = "Allow ${module.function_sign_issuer.function_app.function_app.name} to read secrets from ${data.azurerm_key_vault.sign_weu_kv.name}"
+      has_rbac_support    = false
+      roles = {
+        secrets = "reader"
+      }
+    }
+  ]
+}
+
+module "itn_sign_issuer_func_staging_roles" {
+  source          = "pagopa-dx/azure-role-assignments/azurerm"
+  version         = "~> 1.2.0"
+  principal_id    = module.function_sign_issuer.function_app.function_app.slot.principal_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
+
+  key_vault = [
+    {
+      name                = data.azurerm_key_vault.sign_weu_kv.name
+      resource_group_name = data.azurerm_key_vault.sign_weu_kv.resource_group_name
+      description         = "Allow ${module.function_sign_issuer.function_app.function_app.slot.name} to read secrets from ${data.azurerm_key_vault.sign_weu_kv.name}"
+      has_rbac_support    = false
+      roles = {
+        secrets = "reader"
+      }
+    }
+  ]
+}
