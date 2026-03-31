55b930244520ec0cb0730dea33018b8177f73a85
Stefano D'Aversa <stefano.daversa@pagopa.it>
2026-03-31 12:19:58 +0200

    fix azure web jobs name

--- infra/italynorth/modules/issuer_function_app/main.tf
+++ infra/italynorth/modules/issuer_function_app/main.tf
@@ -29,9 +29,9 @@ module "function_sign_issuer" {
     {
       "AzureWebJobs.closeSignatureRequestRejected.Disabled" = "1"
       "AzureWebJobs.closeSignatureRequestSigned.Disabled"   = "1"
-      "AzureWebJobs.CreateIssuerByVatNumberView.Disabled"   = "1"
-      "AzureWebJobs.MarkAsWaitForSignature.Disabled"        = "1"
-      "AzureWebJobs.ValidateUpload.Disabled"                = "1"
+      "AzureWebJobs.createIssuerByVatNumberView.Disabled"   = "1"
+      "AzureWebJobs.markAsWaitForSignature.Disabled"        = "1"
+      "AzureWebJobs.validateUpload.Disabled"                = "1"
     }
   )
 
@@ -40,9 +40,9 @@ module "function_sign_issuer" {
     {
       "AzureWebJobs.closeSignatureRequestRejected.Disabled" = "1"
       "AzureWebJobs.closeSignatureRequestSigned.Disabled"   = "1"
-      "AzureWebJobs.CreateIssuerByVatNumberView.Disabled"   = "1"
-      "AzureWebJobs.MarkAsWaitForSignature.Disabled"        = "1"
-      "AzureWebJobs.ValidateUpload.Disabled"                = "1"
+      "AzureWebJobs.createIssuerByVatNumberView.Disabled"   = "1"
+      "AzureWebJobs.markAsWaitForSignature.Disabled"        = "1"
+      "AzureWebJobs.validateUpload.Disabled"                = "1"
     }
   )
 
