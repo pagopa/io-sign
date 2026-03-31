c88f5cb146807eab00f383d9a8d53c5deb800e11
Mirco Civolani <mirco.civolani@pagopa.it>
2026-03-31 15:18:35 +0200

    creating function_sign_backoffice in ITN

--- infra/italynorth/prod/local.tf
+++ infra/italynorth/prod/local.tf
@@ -30,4 +30,10 @@ locals {
   function_issuer_autoscale_minimum = 2
   function_issuer_autoscale_maximum = 5
   function_issuer_autoscale_default = 2
+
+  #backoffice function app
+  sign_backoffice_snet_cidr             = "10.20.41.64/26"
+  function_backoffice_autoscale_minimum = 2
+  function_backoffice_autoscale_maximum = 5
+  function_backoffice_autoscale_default = 2
 }
--- infra/italynorth/prod/main.tf
+++ infra/italynorth/prod/main.tf
@@ -20,6 +20,17 @@ module "function_sign_issuer" {
   tags                              = local.tags
 }
 
+module "function_sign_backoffice" {
+  source                                = "../modules/backoffice_function_app"
+  vnet_common_name_itn                  = local.vnet_common_name_itn
+  common_resource_group_name_itn        = local.common_resource_group_name_itn
+  sign_backoffice_snet_cidr             = local.sign_backoffice_snet_cidr
+  function_backoffice_autoscale_minimum = local.function_backoffice_autoscale_minimum
+  function_backoffice_autoscale_maximum = local.function_backoffice_autoscale_maximum
+  function_backoffice_autoscale_default = local.function_backoffice_autoscale_default
+  tags                                  = local.tags
+}
+
 import {
   to = module.function_sign_support.azurerm_resource_group.itn_sign_backend_rg
   id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-sign-backend-rg-01"
