1a7331229c3b2337a439e77b65d7a99d6c15afab
Stefano D'Aversa <stefano.daversa@pagopa.it>
2026-03-31 16:17:15 +0200

    itn user function app with dx modules

---
+++ infra/italynorth/modules/user_function_app/autoscaler.tf
@@ -0,0 +1,67 @@
+module "function_sign_user_autoscale" {
+  source  = "pagopa-dx/azure-app-service-plan-autoscaler/azurerm"
+  version = "~> 2.0"
+
+  resource_group_name = module.function_sign_user.function_app.resource_group_name
+  location            = local.location_itn
+  app_service_plan_id = module.function_sign_user.function_app.plan.id
+  target_service = {
+    function_apps = [
+      { id = module.function_sign_user.function_app.function_app.id }
+    ]
+  }
+
+  scheduler = {
+    normal_load = {
+      minimum = var.function_user_autoscale_minimum
+      default = var.function_user_autoscale_default
+    },
+    maximum = var.function_user_autoscale_maximum
+  }
+
+  scale_metrics = {
+    requests = {
+      #################
+      # scale out
+      #################
+      statistic_increase   = "Max"
+      time_window_increase = 1
+      time_aggregation     = "Maximum"
+      upper_threshold      = 2500
+      increase_by          = 2
+      cooldown_increase    = 3
+      #################
+      # scale in
+      #################
+      statistic_decrease        = "Average"
+      time_window_decrease      = 5
+      time_aggregation_decrease = "Average"
+      lower_threshold           = 500
+      decrease_by               = 1
+      cooldown_decrease         = 3
+    }
+    cpu = {
+      #################
+      # scale out
+      #################
+      statistic_increase        = "Max"
+      time_window_increase      = 1
+      time_aggregation_increase = "Maximum"
+      upper_threshold           = 60
+      increase_by               = 2
+      cooldown_increase         = 3
+      #################
+      # scale in
+      #################
+      statistic_decrease        = "Average"
+      time_window_decrease      = 5
+      time_aggregation_decrease = "Average"
+      lower_threshold           = 25
+      decrease_by               = 1
+      cooldown_decrease         = 3
+    }
+    memory = null
+  }
+
+  tags = var.tags
+}
--- infra/italynorth/prod/main.tf
+++ infra/italynorth/prod/main.tf
@@ -20,7 +20,13 @@ module "function_sign_issuer" {
   tags                              = local.tags
 }
 
-import {
-  to = module.function_sign_support.azurerm_resource_group.itn_sign_backend_rg
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-sign-backend-rg-01"
+module "function_sign_user" {
+  source                          = "../modules/user_function_app"
+  vnet_common_name_itn            = local.vnet_common_name_itn
+  common_resource_group_name_itn  = local.common_resource_group_name_itn
+  sign_user_snet_cidr             = local.sign_user_snet_cidr
+  function_user_autoscale_minimum = local.function_user_autoscale_minimum
+  function_user_autoscale_maximum = local.function_user_autoscale_maximum
+  function_user_autoscale_default = local.function_user_autoscale_default
+  tags                            = local.tags
 }
