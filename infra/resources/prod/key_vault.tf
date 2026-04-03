76905898af4bcd39c6712a14981ca59a6f7355bc
Mirco Civolani <mirco.civolani@pagopa.it>
2026-04-03 10:15:42 +0200

    switch to itn func

--- infra/resources/prod/key_vault.tf
+++ infra/resources/prod/key_vault.tf
@@ -28,7 +28,8 @@ module "key_vault_secrets" {
     "PdvTokenizerApiKey",
     "BackOfficeApiKey",
     "io-services-configuration-id",
-    "io-sign-backoffice-func-key"
+    "io-sign-backoffice-func-key",
+    "io-sign-backoffice-func-key-itn"
   ]
 }
 
