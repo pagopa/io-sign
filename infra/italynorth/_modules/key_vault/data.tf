e351d61f8a47e5da84a596282bd20989a7d8acff
Iustin <iustinburlacu@gmail.com>
2026-04-15 12:45:57 +0200

    move azure groups and role assignments to bootstrapper

--- infra/italynorth/_modules/key_vault/data.tf
+++ infra/italynorth/_modules/key_vault/data.tf
@@ -1,19 +1,1 @@
 data "azurerm_client_config" "current" {}
-
-data "azurerm_subscription" "current" {}
-
-data "azuread_group" "adgroup_admin" {
-  display_name = format("%s-adgroup-admin", local.product)
-}
-
-data "azuread_group" "adgroup_developers" {
-  display_name = format("%s-adgroup-developers", local.product)
-}
-
-data "azuread_group" "adgroup_sign" {
-  display_name = format("%s-adgroup-sign", local.product)
-}
-
-data "azuread_group" "adgroup_ecosystem_n_links" {
-  display_name = format("%s-adgroup-ecosystem-n-links-admins", local.product)
-}
