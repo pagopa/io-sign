2b5111068f37cf6379210fbe0c691725eecd59b7
Iustin <iustinburlacu@gmail.com>
2026-04-16 10:42:53 +0200

    add rbac to backoffice app to access to kv in ITN

--- infra/italynorth/_modules/backoffice_app_service/data.tf
+++ infra/italynorth/_modules/backoffice_app_service/data.tf
@@ -42,3 +42,8 @@ data "azurerm_key_vault" "sign_weu_kv" {
   name                = "${local.project_weu_sign}-kv"
   resource_group_name = "${local.project_weu_sign}-sec-rg"
 }
+
+data "azurerm_key_vault" "sign_kv" {
+  name                = "${local.project_itn_sign}-kv-${local.instance_number}"
+  resource_group_name = "${local.project_itn_sign}-rg-${local.instance_number}"
+}
