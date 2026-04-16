119c5fd39973b51b35903526ed09aa117ad07a5c
Iustin <iustinburlacu@gmail.com>
2026-04-16 10:54:46 +0200

    remove ref to kv weu

--- infra/italynorth/_modules/backoffice_app_service/main.tf
+++ infra/italynorth/_modules/backoffice_app_service/main.tf
@@ -104,15 +104,6 @@ module "itn_sign_backoffice_app_roles" {
 
   key_vault = [
     {
-      name                = data.azurerm_key_vault.sign_weu_kv.name
-      resource_group_name = data.azurerm_key_vault.sign_weu_kv.resource_group_name
-      description         = "Allow ${module.itn_sign_backoffice_app.app_service.app_service.name} to read secrets from ${data.azurerm_key_vault.sign_weu_kv.name}"
-      has_rbac_support    = false
-      roles = {
-        secrets = "reader"
-      }
-    },
-    {
       name                = data.azurerm_key_vault.sign_kv.name
       resource_group_name = data.azurerm_key_vault.sign_kv.resource_group_name
       description         = "Allow ${module.itn_sign_backoffice_app.app_service.app_service.name} to read secrets from ${data.azurerm_key_vault.sign_kv.name}"
@@ -131,15 +122,6 @@ module "itn_sign_backoffice_app_staging_roles" {
   subscription_id = data.azurerm_subscription.current.subscription_id
 
   key_vault = [
-    {
-      name                = data.azurerm_key_vault.sign_weu_kv.name
-      resource_group_name = data.azurerm_key_vault.sign_weu_kv.resource_group_name
-      description         = "Allow ${module.itn_sign_backoffice_app.app_service.app_service.slot.name} to read secrets from ${data.azurerm_key_vault.sign_weu_kv.name}"
-      has_rbac_support    = false
-      roles = {
-        secrets = "reader"
-      }
-    },
     {
       name                = data.azurerm_key_vault.sign_kv.name
       resource_group_name = data.azurerm_key_vault.sign_kv.resource_group_name
