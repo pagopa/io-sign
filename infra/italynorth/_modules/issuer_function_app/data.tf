a37a056422311427f97fffbd80346a3f7b4df05a
Iustin <iustinburlacu@gmail.com>
2026-04-16 11:17:59 +0200

    add rbac to issuer func to access to kv in ITN

--- infra/italynorth/_modules/issuer_function_app/data.tf
+++ infra/italynorth/_modules/issuer_function_app/data.tf
@@ -4,6 +4,11 @@ data "azurerm_key_vault" "sign_weu_kv" {
   resource_group_name = "${local.project_weu_sign}-sec-rg"
 }
 
+data "azurerm_key_vault" "sign_kv" {
+  name                = "${local.project_itn_sign}-kv-${local.instance_number}"
+  resource_group_name = "${local.project_itn_sign}-rg-${local.instance_number}"
+}
+
 data "azurerm_resource_group" "sign_itn_rg" {
   name = "${local.project_itn_sign}-rg-01"
 }
