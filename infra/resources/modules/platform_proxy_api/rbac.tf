ca2bf4111159f1649d4b8ceec6c2dee40cf485ac
Iustin <iustinburlacu@gmail.com>
2026-04-17 11:18:12 +0200

    revert rbac access to apim for kv itn

--- infra/resources/modules/platform_proxy_api/rbac.tf
+++ infra/resources/modules/platform_proxy_api/rbac.tf
@@ -13,15 +13,6 @@ module "apim_platform_roles" {
       roles = {
         secrets = "reader"
       }
-    },
-    {
-      name                = var.key_vault_common_name_itn
-      resource_group_name = var.key_vault_resource_group_name_itn
-      description         = "Allow ${var.platform_apim_name} to read secrets from ${var.key_vault_common_name_itn} with rbac"
-      has_rbac_support    = true
-      roles = {
-        secrets = "reader"
-      }
     }
   ]
 }
