2372bc5dbe3dcfe22ab01413435b0ee70fa8d435
Iustin <iustinburlacu@gmail.com>
2026-04-20 14:17:12 +0200

    remove rg sec from weu

--- infra/resources/prod/resource_groups.tf
+++ infra/resources/prod/resource_groups.tf
@@ -19,13 +19,6 @@ resource "azurerm_resource_group" "backend_rg" {
   tags = var.tags
 }
 
-resource "azurerm_resource_group" "sec_rg" {
-  name     = format("%s-sec-rg", local.project)
-  location = var.location
-
-  tags = var.tags
-}
-
 # Needed to integrate Firma con IO with external domains, products or platforms (ie. eventhub for billing, ...)
 resource "azurerm_resource_group" "integration_rg" {
   name     = format("%s-integration-rg", local.project)
