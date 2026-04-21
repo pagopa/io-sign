60eaad11ca48e8bf72ae35377726d89c543a330b
Mirco Civolani <mirco.civolani@outlook.it>
2026-04-21 17:28:56 +0200

    Revert user-func to Node v20 (#565)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    Revert user-func to Node v20
    <!--- Describe your changes in detail -->
    
    #### Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    <!--- Attach screenshots in case changes impact UI. -->
    
    #### Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

--- infra/italynorth/_modules/user_function_app/main.tf
+++ infra/italynorth/_modules/user_function_app/main.tf
@@ -12,7 +12,7 @@ module "function_sign_user" {
 
   resource_group_name = data.azurerm_resource_group.sign_itn_rg.name
   size                = "P0v3"
-  node_version        = "22"
+  node_version        = "20"
 
   virtual_network = {
     name                = var.vnet_common_name_itn
