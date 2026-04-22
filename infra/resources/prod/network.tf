94e367454b35269bba74ae30e2949f8e1fd12a70
Stefano D'Aversa <stefano.daversa@pagopa.it>
2026-04-22 15:43:41 +0200

    remove unused data

--- infra/resources/prod/network.tf
+++ infra/resources/prod/network.tf
@@ -20,12 +20,6 @@ data "azurerm_subnet" "itn_private_endpoints_subnet" {
   resource_group_name  = data.azurerm_virtual_network.itn_vnet_common.resource_group_name
 }
 
-data "azurerm_subnet" "apim_itn" {
-  name                 = "${local.project_itn}-apim-snet-01"
-  virtual_network_name = "${local.project_itn}-common-vnet-01"
-  resource_group_name  = "${local.project_itn}-common-rg-01"
-}
-
 data "azurerm_nat_gateway" "nat_gateway" {
   name                = format("%s-natgw", local.product)
   resource_group_name = format("%s-rg-common", local.product)
