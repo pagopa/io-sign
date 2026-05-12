f75e9afa85ff41680b1525219dca07c9cf578fcf
Stefano D'Aversa <stefano.daversa@pagopa.it>
2026-05-12 14:05:22 +0200

    Update infra/italynorth/_modules/event_hub/data.tf
    Co-authored-by: Mirco Civolani <mirco.civolani@outlook.it>

--- infra/italynorth/_modules/event_hub/data.tf
+++ infra/italynorth/_modules/event_hub/data.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "sign_itn_rg" {
 }
 
 data "azurerm_subnet" "private_endpoints_subnet_itn" {
-  name                 = "io-p-itn-pep-snet-01"
+  name                 = "${local.project_itn}-pep-snet-01"
   virtual_network_name = var.vnet_common_name_itn
   resource_group_name  = var.common_resource_group_name_itn
 }
