ff5ceebe7fd83b9186f93299fce5c980fb06e594
Iustin <iustinburlacu@gmail.com>
2026-05-13 14:12:37 +0200

    add ref to kv

--- infra/italynorth/_modules/event_hub/data.tf
+++ infra/italynorth/_modules/event_hub/data.tf
@@ -16,3 +16,24 @@ data "azurerm_monitor_action_group" "common_error_action_group" {
   name                = "${local.prefix}${local.env_short}error"
   resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
 }
+
+data "azurerm_key_vault" "sign_kv" {
+  name                = "${local.project_itn_sign}-kv-${local.instance_number}"
+  resource_group_name = "${local.project_itn_sign}-rg-${local.instance_number}"
+}
+
+data "azurerm_eventhub_authorization_rule" "billing_func" {
+  depends_on          = [module.eventhub]
+  name                = "io-sign-func-sender"
+  namespace_name      = "${local.project_itn_sign}-evhns-${local.instance_number}"
+  eventhub_name       = "billing"
+  resource_group_name = data.azurerm_resource_group.sign_itn_rg.name
+}
+
+data "azurerm_eventhub_authorization_rule" "analytics_func" {
+  depends_on          = [module.eventhub]
+  name                = "io-sign-func-sender"
+  namespace_name      = "${local.project_itn_sign}-evhns-${local.instance_number}"
+  eventhub_name       = "analytics"
+  resource_group_name = data.azurerm_resource_group.sign_itn_rg.name
+}
