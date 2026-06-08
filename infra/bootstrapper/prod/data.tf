9552d49bd3265f3569c92c45b34e4de6afece8d5
Mirco Civolani <mirco.civolani@pagopa.it>
2026-06-08 12:14:22 +0200

    Remove old azurerm_virtual_network

--- infra/bootstrapper/prod/data.tf
+++ infra/bootstrapper/prod/data.tf
@@ -12,11 +12,6 @@ data "azurerm_key_vault" "common" {
   resource_group_name = local.key_vault.resource_group_name
 }
 
-data "azurerm_virtual_network" "common" {
-  name                = local.vnet.name
-  resource_group_name = local.vnet.resource_group_name
-}
-
 data "azurerm_resource_group" "dns_zones" {
   name = local.dns_zones.resource_group_name
 }
