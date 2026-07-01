3ff32cb81c3777e82098081ceed3add70b20b759
Iustin <iustinburlacu@gmail.com>
2026-07-01 12:49:54 +0200

    set node to v24

--- infra/italynorth/_modules/user_function_app_02/main.tf
+++ infra/italynorth/_modules/user_function_app_02/main.tf
@@ -12,7 +12,7 @@ module "function_sign_user_02" {
 
   resource_group_name = data.azurerm_resource_group.sign_itn_rg.name
   size                = "P0v3"
-  node_version        = "22"
+  node_version        = "24"
 
   virtual_network = {
     name                = var.vnet_common_name_itn
