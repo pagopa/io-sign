f9de59b4eac78cedb6cc0b95bbaf44a250ca4dd2
Iustin <iustinburlacu@gmail.com>
2026-07-01 09:25:59 +0200

    add creation infra resources for user-func-02

--- infra/italynorth/prod/local.tf
+++ infra/italynorth/prod/local.tf
@@ -38,6 +38,12 @@ locals {
   function_user_autoscale_maximum = 5
   function_user_autoscale_default = 2
 
+  #user function app 02
+  sign_user_02_snet_cidr             = "10.20.43.0/26"
+  function_user_02_autoscale_minimum = 2
+  function_user_02_autoscale_maximum = 5
+  function_user_02_autoscale_default = 2
+
   #backoffice function app
   sign_backoffice_snet_cidr             = "10.20.41.64/26"
   function_backoffice_autoscale_minimum = 2
