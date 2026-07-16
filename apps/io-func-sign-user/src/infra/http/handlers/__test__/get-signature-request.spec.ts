05d236ac5f6969dc3b4107a2e062a0c527510320
Iustin <iustinburlacu@gmail.com>
2026-07-16 11:58:06 +0200

    fix unit test

--- apps/io-func-sign-user/src/infra/http/handlers/__test__/get-signature-request.spec.ts
+++ apps/io-func-sign-user/src/infra/http/handlers/__test__/get-signature-request.spec.ts
@@ -5,7 +5,12 @@ import * as H from "@pagopa/handler-kit";
 import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/TaskEither";
 import { newId } from "@io-sign/io-sign/id";
-import { EmailString, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import {
+  EmailString,
+  FiscalCode,
+  NonEmptyString,
+} from "@pagopa/ts-commons/lib/strings";
+import { SignerRepository } from "@io-sign/io-sign/signer";
 import {
   SignatureRequest,
   SignatureRequestRepository,
@@ -16,8 +21,10 @@ import { BaseContainerClientWithFallback } from "@pagopa/azure-storage-migration
 
 describe("GetSignatureRequestHandler", () => {
   let signatureRequestRepository: SignatureRequestRepository;
+  let signerRepository: SignerRepository;
 
   const signer = { id: newId() };
+  const fiscalCode = "RSSMRA85T10A562S" as FiscalCode;
 
   const signatureRequest: SignatureRequest = {
     id: newId(),
@@ -55,9 +62,16 @@ describe("GetSignatureRequestHandler", () => {
           : TE.right(O.none),
       upsert: () => TE.left(new Error("not implemented")),
     };
+    signerRepository = {
+      getSignerByFiscalCode: (fc) =>
+        fc === fiscalCode
+          ? TE.right(signer)
+          : TE.left(new Error("unknown fiscal code")),
+      getFiscalCodeBySignerId: () => TE.left(new Error("not implemented")),
+    };
   });
 
-  it("should return a 400 HTTP response when x-iosign-signer-id header is not present", () => {
+  it("should return a 400 HTTP response when x-iosign-fiscal-code header is not present", () => {
     const req: H.HttpRequest = {
       ...H.request("https://api.test.it/"),
       path: {
@@ -69,6 +83,7 @@ describe("GetSignatureRequestHandler", () => {
     };
     const run = GetSignatureRequestHandler({
       signatureRequestRepository,
+      signerRepository,
       logger,
       inputDecoder: H.HttpRequest,
       input: req,
@@ -94,11 +109,12 @@ describe("GetSignatureRequestHandler", () => {
         signatureRequestId: newId(),
       },
       headers: {
-        "x-iosign-signer-id": mocks.signer.id,
+        "x-iosign-fiscal-code": fiscalCode,
       },
     };
     const run = GetSignatureRequestHandler({
       signatureRequestRepository,
+      signerRepository,
       logger,
       inputDecoder: H.HttpRequest,
       input: req,
@@ -124,11 +140,12 @@ describe("GetSignatureRequestHandler", () => {
         signatureRequestId: mocks.signatureRequest.id,
       },
       headers: {
-        "x-iosign-signer-id": mocks.signer.id,
+        "x-iosign-fiscal-code": fiscalCode,
       },
     };
     const run = GetSignatureRequestHandler({
       signatureRequestRepository,
+      signerRepository,
       logger,
       inputDecoder: H.HttpRequest,
       input: req,
