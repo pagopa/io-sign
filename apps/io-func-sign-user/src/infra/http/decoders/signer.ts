7fcc0874e43becb2cfede16ca8b9b59396950fe7
Iustin <iustinburlacu@gmail.com>
2026-07-31 12:08:11 +0200

    remove requireSigner method

--- apps/io-func-sign-user/src/infra/http/decoders/signer.ts
+++ apps/io-func-sign-user/src/infra/http/decoders/signer.ts
@@ -1,4 +1,4 @@
-import { flow, pipe } from "fp-ts/lib/function";
+import { pipe } from "fp-ts/lib/function";
 import { lookup } from "fp-ts/Record";
 import * as E from "fp-ts/lib/Either";
 import * as RTE from "fp-ts/lib/ReaderTaskEither";
@@ -40,27 +40,6 @@ export const requireSpidLevel = (req: H.HttpRequest) =>
       )
     )
   );
-
-export const requireSignerId = (req: H.HttpRequest) =>
-  pipe(
-    req.headers,
-    lookup("x-iosign-signer-id"),
-    E.fromOption(
-      () => new H.HttpBadRequestError("Missing x-iosign-signer-id in header")
-    ),
-    E.chainW(
-      H.parse(
-        Signer.props.id,
-        "The content of x-iosign-signer-id is not a valid id"
-      )
-    )
-  );
-
-export const requireSigner = flow(
-  requireSignerId,
-  E.map((id) => ({ id })),
-  E.chainW(H.parse(Signer, "Cannot parse the given object to a Signer"))
-);
 
 export const requireSignerIdFromFiscalCode = (
   req: H.HttpRequest
