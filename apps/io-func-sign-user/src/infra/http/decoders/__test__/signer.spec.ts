fa5b42dee62c49d4d1fc6c69d0c664f6d6986ee5
Iustin <iustinburlacu@gmail.com>
2026-07-31 12:17:38 +0200

    fix unit test requireSignerId

--- apps/io-func-sign-user/src/infra/http/decoders/__test__/signer.spec.ts
+++ apps/io-func-sign-user/src/infra/http/decoders/__test__/signer.spec.ts
@@ -1,35 +1,8 @@
-import { newId } from "@io-sign/io-sign/id";
 import { it, describe, expect } from "vitest";
 
 import * as H from "@pagopa/handler-kit";
 
-import { requireSignerId, requireSpidLevel } from "../signer";
-
-describe("requireSignerId", () => {
-  it('should correctly parse the "x-iosign-signer-id" header', () => {
-    const signerId = newId();
-    const req: H.HttpRequest = {
-      ...H.request("my-url"),
-      headers: { "x-iosign-signer-id": signerId },
-    };
-    expect(requireSignerId(req)).toEqual(
-      expect.objectContaining({
-        right: signerId,
-      })
-    );
-  });
-  it("fails with an Http Bad Request error when header is not found", () => {
-    const req = H.request("my-url");
-    expect(requireSignerId(req)).toEqual(
-      expect.objectContaining({
-        left: expect.objectContaining({
-          name: "HttpError",
-          status: 400,
-        }),
-      })
-    );
-  });
-});
+import { requireSpidLevel } from "../signer";
 
 describe("requireSpidLevel", () => {
   it("returns the SPID level when the header is SpidL3", () => {
