91c99b4b593f441972ff79a9d2c80cc99b000d61
Iustin <iustinburlacu@gmail.com>
2026-08-04 14:44:08 +0200

    remove legacy code

--- apps/io-func-sign-user-legacy/src/infra/http/encoders/signer.ts
+++
@@ -1,8 +0,0 @@
-import * as E from "io-ts/lib/Encoder";
-
-import { Signer } from "@io-sign/io-sign/signer";
-import { SignerDetailView as SignerApiModel } from "../models/SignerDetailView";
-
-export const SignerToApiModel: E.Encoder<SignerApiModel, Signer> = {
-  encode: ({ id }) => ({ id })
-};
