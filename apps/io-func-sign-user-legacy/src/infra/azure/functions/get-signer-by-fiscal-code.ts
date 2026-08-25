4fcabd3915727f8667953f2c5ffdba1869908175
Iustin-Burlacu <50848825+Iustin-Burlacu@users.noreply.github.com>
2026-08-25 16:17:18 +0200

    Remove user func legacy (#735)
    Actions: 
    - remove the legacy `apps/io-func-sign-user-legacy` app and its related
    code
    - remove the legacy `user_function_app` Terraform module and its
    variables from `infra/italynorth`
    - remove the docker compose service, scripts, and env vars dedicated to
    `io-func-sign-user-legacy`
    - remove the deploy GHA for the user app legacy
    - remove the `io-func-sign-user-legacy` entry from `.github/labeler.yml`
    - update `pnpm-lock.yaml`
    - remove the unused `openapi.yaml` from `apps/io-func-sign-user` (now
    split into api/external.yaml (HTTP client) and api/internal.yaml
    (types))
    - update the trigger path in `.github/workflows/opex-dashboards.yaml` to
    point to the new `apps/io-func-sign-user/api/external.yaml`
    
    Resolves IEL-626
    
    Depends on https://github.com/pagopa/io-infra/pull/1982
    
    ---------
    
    Co-authored-by: Mirco Civolani <mirco.civolani@pagopa.it>

--- apps/io-func-sign-user-legacy/src/infra/azure/functions/get-signer-by-fiscal-code.ts
+++
@@ -1,6 +0,0 @@
-import { httpAzureFunction } from "@pagopa/handler-kit-azure-func";
-import { GetSignerByFiscalCodeHandler } from "../../http/handlers/get-signer-by-fiscal-code";
-
-export const GetSignerByFiscalCodeFunction = httpAzureFunction(
-  GetSignerByFiscalCodeHandler
-);
