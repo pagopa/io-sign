7d288db259260a4a891b8f83cf4bc5a2504fa93a
Mirco Civolani <mirco.civolani@pagopa.it>
2026-09-01 12:27:16 +0200

    chore: remove dead analytics/billing EventHub config from issuer and user funcs
    Both connection strings are no longer used after routing events through
    the sign-events pipeline. io-func-sign-events has its own separate config.

--- infra/italynorth/_modules/user_function_app_02/local.tf
+++ infra/italynorth/_modules/user_function_app_02/local.tf
@@ -32,8 +32,6 @@ locals {
       NamirialTestApiBasePath           = "https://pagopa-test.namirial.com"
       NamirialTestUsername              = "api"
       NamirialTestPassword              = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.sign_kv.name};SecretName=NamirialTestPassword)"
-      AnalyticsEventHubItnConnectionString = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.sign_kv.name};SecretName=AnalyticsEventHubItnConnectionString)"
-      BillingEventHubItnConnectionString   = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.sign_kv.name};SecretName=BillingEventHubItnConnectionString)"
       SignEventsHubSenderItnConnectionString = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.sign_kv.name};SecretName=SignEventsHubSenderItnConnectionString)"
       SelfCareEventHubConnectionString  = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.sign_kv.name};SecretName=SelfCareEventHubConnectionString)"
       SelfCareApiBasePath               = "https://api.selfcare.pagopa.it"
