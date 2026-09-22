b3bbe61b6c4d44852c0072df862854e8dcbebc78
Stefano D'Aversa <stefano.daversa@pagopa.it>
2026-09-22 15:43:29 +0200

    add remove blocks for resources with status imported in itn

--- infra/resources/prod/resource_groups.tf
+++ infra/resources/prod/resource_groups.tf
@@ -1,10 +1,3 @@
-resource "azurerm_resource_group" "data_rg" {
-  name     = format("%s-data-rg", local.project)
-  location = var.location
-
-  tags = var.tags
-}
-
 resource "azurerm_resource_group" "sign" {
   name     = format("%s-itn-sign-rg-01", local.product)
   location = "italynorth"
@@ -12,10 +5,3 @@ resource "azurerm_resource_group" "sign" {
   tags = var.tags
 }
 
-# Needed to integrate Firma con IO with external domains, products or platforms (ie. eventhub for billing, ...)
-resource "azurerm_resource_group" "integration_rg" {
-  name     = format("%s-integration-rg", local.project)
-  location = var.location
-
-  tags = var.tags
-}
