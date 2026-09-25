2d1a7779e7aae1b0526bb434bc071a2670f09241
Stefano D'Aversa <stefano.daversa@pagopa.it>
2026-09-25 12:00:29 +0200

    clean resources after removing tf state

--- infra/resources/prod/landing.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_resource_group" "core_ext" {
-  name = format("%s-rg-external", local.product)
-}
-
-data "azurerm_dns_zone" "io_italia_it" {
-  name                = "io.italia.it"
-  resource_group_name = data.azurerm_resource_group.core_ext.name
-}
-
